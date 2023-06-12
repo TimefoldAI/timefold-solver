a4a959a249bfc29e92eda463d2f69ad1f49456a3
Lukáš Petrovický <lukas@petrovicky.net>
2023-06-12 12:13:45 +0200

    ci: run quickstarts in PR checks
    Signed-off-by: Lukáš Petrovický <lukas@petrovicky.net>

---
+++ .github/scripts/check_chain_repo.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Expects the following environment variables to be set:
+#   $CHAIN_USER              (Example: "triceo")
+#   $CHAIN_BRANCH            (Example: "my_feature_branch")
+#   $CHAIN_REPO              (Example: "timefold-quickstarts")
+#   $CHAIN_DEFAULT_BRANCH    (Example: "development")
+#
+# When it terminates, the following Github environment variables will have been set:
+#   $TARGET_CHAIN_USER       (Example: "triceo")
+#   $TARGET_CHAIN_REPO       (Example: "timefold-quickstarts")
+#   $TARGET_CHAIN_BRANCH     (Example: "my_feature_branch")
+
+# Check if the user has cloned the chained repo.
+FULL_CHAIN_REPO="https://github.com/$CHAIN_USER/$CHAIN_REPO.git"
+echo "Checking if a branch '$CHAIN_BRANCH' exists in '$FULL_CHAIN_REPO'..."
+git ls-remote --exit-code $FULL_CHAIN_REPO --heads $CHAIN_BRANCH
+if [ $? -ne 0 ]; then
+    echo "Branch '$CHAIN_BRANCH' in '$FULL_CHAIN_REPO' does not exist."
+    echo "TARGET_CHAIN_USER=TimefoldAI" >> "$GITHUB_ENV"
+    echo "TARGET_CHAIN_BRANCH=$CHAIN_DEFAULT_BRANCH" >> "$GITHUB_ENV"
+else
+    echo "Branch '$CHAIN_BRANCH' in '$FULL_CHAIN_REPO' exists."
+    echo "TARGET_CHAIN_USER=$CHAIN_USER" >> "$GITHUB_ENV"
+    echo "TARGET_CHAIN_BRANCH=$CHAIN_BRANCH" >> "$GITHUB_ENV"
+fi
+echo "TARGET_CHAIN_REPO=$CHAIN_REPO" >> "$GITHUB_ENV"
+echo "Will use branch '$TARGET_CHAIN_BRANCH' from 'https://github.com/$TARGET_CHAIN_USER/$TARGET_CHAIN_REPO.git'"
