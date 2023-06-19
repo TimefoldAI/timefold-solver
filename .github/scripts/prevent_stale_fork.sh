0e0ca2224baa1d103599e1334f5845f508dd383c
Lukáš Petrovický <lukas@petrovicky.net>
2023-06-19 14:14:56 +0200

    Still continued experiments
    Signed-off-by: Lukáš Petrovický <lukas@petrovicky.net>

--- .github/scripts/prevent_stale_fork.sh
+++ .github/scripts/prevent_stale_fork.sh
@@ -13,11 +13,15 @@ git config --local user.email "ci@timefold.ai"
 # Rebase the fork onto the latest.
 echo "Will rebase current branch onto '$BLESSED_BRANCH' from 'https://github.com/TimefoldAI/$BLESSED_REPO.git'"
 git remote add upstream https://github.com/TimefoldAI/$BLESSED_REPO.git
-git fetch upstream
-git merge upstream/$BLESSED_BRANCH --allow-unrelated-histories
+git fetch upstream $BLESSED_BRANCH
+git merge upstream/$BLESSED_BRANCH
 
 if [ $? -ne 0 ]; then
-    echo "Rebase failed, aborting."
-    echo "Please make sure your fork is up to date."
-    exit 1
+    echo "Merge failed, trying rebase."
+    git rebase upstream/$BLESSED_BRANCH
+    if [ $? -ne 0 ]; then
+      echo "Rebase failed too."
+      echo "Please make sure your fork is up to date."
+      exit 1
+    fi
 fi
