2e6756df75e69208130da71bfbd246d19a9a783f
Lukáš Petrovický <lukas@petrovicky.net>
2023-09-05 10:09:28 +0200

    fix: stop using the LambdaMetaFactory

--- core/core-impl/src/main/java/ai/timefold/solver/core/impl/domain/common/accessor/MemberAccessorFactory.java
+++ core/core-impl/src/main/java/ai/timefold/solver/core/impl/domain/common/accessor/MemberAccessorFactory.java
@@ -4,7 +4,6 @@ import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.Member;
 import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
 import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
@@ -18,8 +17,9 @@ import ai.timefold.solver.core.impl.domain.common.accessor.gizmo.GizmoMemberAcce
 public final class MemberAccessorFactory {
 
     // exists only so that the various member accessors can share the same text in their exception messages
-    static final String CLASSLOADER_NUDGE_MESSAGE = "Maybe add getClass().getClassLoader() as a parameter to the " +
-            SolverFactory.class.getSimpleName() + ".create...() method call.";
+    static final String CLASSLOADER_NUDGE_MESSAGE =
+            "Maybe add getClass().getClassLoader() as a parameter to the %s.create...() method call."
+                    .formatted(SolverFactory.class.getSimpleName());
 
     /**
      * Creates a new member accessor based on the given parameters.
@@ -62,14 +62,7 @@ public final class MemberAccessorFactory {
                 case FIELD_OR_GETTER_METHOD_WITH_SETTER:
                     boolean getterOnly = memberAccessorType != MemberAccessorType.FIELD_OR_GETTER_METHOD_WITH_SETTER;
                     ReflectionHelper.assertGetterMethod(method, annotationClass);
-                    if (Modifier.isPublic(method.getModifiers())
-                            // HACK The lambda approach doesn't support classes from another classloader in JDK 8
-                            // TODO In JDK 9 use MethodHandles.privateLookupIn(Class, MethodHandles.lookup())
-                            && method.getDeclaringClass().getClassLoader().equals(MemberAccessor.class.getClassLoader())) {
-                        memberAccessor = new LambdaBeanPropertyMemberAccessor(method, getterOnly);
-                    } else {
-                        memberAccessor = new ReflectionBeanPropertyMemberAccessor(method, getterOnly);
-                    }
+                    memberAccessor = new ReflectionBeanPropertyMemberAccessor(method, getterOnly);
                     break;
                 default:
                     throw new IllegalStateException("The memberAccessorType (" + memberAccessorType
