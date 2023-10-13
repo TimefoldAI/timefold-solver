6ecafb92fafa6a3213c1fd1741bb2f4b58a6225e
Lukáš Petrovický <lukas@petrovicky.net>
2023-10-13 19:12:34 +0200

    Update core/core-impl/src/main/java/ai/timefold/solver/core/impl/domain/solution/descriptor/SolutionDescriptor.java

--- core/core-impl/src/main/java/ai/timefold/solver/core/impl/domain/solution/descriptor/SolutionDescriptor.java
+++ core/core-impl/src/main/java/ai/timefold/solver/core/impl/domain/solution/descriptor/SolutionDescriptor.java
@@ -572,7 +572,7 @@ public class SolutionDescriptor<Solution_> {
                     "Combining basic variables (%s) with list variables (%s) on a single planning entity (%s)" +
                             " is currently not supported."
                             .formatted(basicVariableDescriptors, listVariableDescriptor,
-                                    listVariableDescriptor.getEntityDescriptor().getEntityClass().getName()));
+                                    listVariableDescriptor.getEntityDescriptor().getEntityClass().getCanonicalName()));
         }
     }
 
