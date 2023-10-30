d881540ef7ae0830a2b949a92f2ccfd6e99291e4
Christopher Chianelli <christopher@timefold.ai>
2023-10-30 13:29:02 -0400

    docs: Update ConstantLambdaUtils' JavaDoc
    Co-authored-by: Lukáš Petrovický <lukas@petrovicky.net>

--- core/core-impl/src/main/java/ai/timefold/solver/core/impl/util/ConstantLambdaUtils.java
+++ core/core-impl/src/main/java/ai/timefold/solver/core/impl/util/ConstantLambdaUtils.java
@@ -13,7 +13,7 @@ import ai.timefold.solver.core.api.function.ToIntTriFunction;
 import ai.timefold.solver.core.api.function.TriFunction;
 
 /**
- * A class that hold common lambdas that are guarantee to be the same across method calls. In most JDK's, stateless lambdas are
+ * A class that holds common lambdas that are guaranteed to be the same across method calls. In most JDK's, stateless lambdas are
  * bound to a {@link java.lang.invoke.ConstantCallSite} inside the method that define them, but that
  * {@link java.lang.invoke.ConstantCallSite} is not shared across methods (even for methods in the same class). Thus, when
  * lambda reference equality is important (such as for node sharing in Constraint Streams), the lambdas in this class should be
