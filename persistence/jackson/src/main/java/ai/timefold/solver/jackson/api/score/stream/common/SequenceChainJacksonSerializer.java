1e97a72828f035011d55250342e178ce63087ee0
Lukáš Petrovický <lukas@petrovicky.net>
2023-11-27 10:02:53 +0100

    Optimize

--- persistence/jackson/src/main/java/ai/timefold/solver/jackson/api/score/stream/common/SequenceChainJacksonSerializer.java
+++ persistence/jackson/src/main/java/ai/timefold/solver/jackson/api/score/stream/common/SequenceChainJacksonSerializer.java
@@ -1,11 +1,12 @@
 package ai.timefold.solver.jackson.api.score.stream.common;
 
 import java.io.IOException;
-import java.util.IdentityHashMap;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
+import ai.timefold.solver.core.api.score.stream.common.Break;
 import ai.timefold.solver.core.api.score.stream.common.SequenceChain;
+import ai.timefold.solver.core.impl.util.CollectionUtils;
 
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
@@ -17,20 +18,28 @@ public final class SequenceChainJacksonSerializer<Value_, Difference_ extends Co
     @Override
     public void serialize(SequenceChain<Value_, Difference_> sequenceChain, JsonGenerator jsonGenerator,
             SerializerProvider serializerProvider) throws IOException {
-        var breakToSerializedBreakMap = sequenceChain.getBreaks()
-                .stream()
-                .collect(Collectors.toMap(b -> b, SerializedBreak::of, (b1, b2) -> b1, IdentityHashMap::new));
-        var serializedSequenceList = sequenceChain.getConsecutiveSequences()
-                .stream()
-                .map(s -> new SerializedSequence<>(breakToSerializedBreakMap.get(s.getPreviousBreak()),
-                        breakToSerializedBreakMap.get(s.getNextBreak()),
-                        List.copyOf(s.getItems())))
-                .toList();
-        jsonGenerator.writeObject(new SerializedSequenceChain<>(serializedSequenceList,
-                // The map does not maintain consistent insertion order, so we create a new list.
-                sequenceChain.getBreaks().stream()
-                        .map(breakToSerializedBreakMap::get)
-                        .toList()));
+        // Convert breaks to a serializable format.
+        var breakCount = sequenceChain.getBreaks().size();
+        var breakToSerializedBreakMap =
+                CollectionUtils.<Break<Value_, Difference_>, SerializedBreak<Value_>> newIdentityHashMap(breakCount);
+        var serializedBreakList = new ArrayList<SerializedBreak<Value_>>(breakCount);
+        for (var brk : sequenceChain.getBreaks()) {
+            var serializedBreak = SerializedBreak.of(brk);
+            serializedBreakList.add(serializedBreak);
+            breakToSerializedBreakMap.putIfAbsent(brk, serializedBreak);
+        }
+        // Convert sequences to a serializable format.
+        var serializedSequenceList =
+                new ArrayList<SerializedSequence<Value_>>(sequenceChain.getConsecutiveSequences().size());
+        for (var sequence : sequenceChain.getConsecutiveSequences()) {
+            // Make sure to reuse the same serialized break instances for the same break instances.
+            var serializedSequence = new SerializedSequence<>(
+                    breakToSerializedBreakMap.get(sequence.getPreviousBreak()),
+                    breakToSerializedBreakMap.get(sequence.getNextBreak()),
+                    List.copyOf(sequence.getItems()));
+            serializedSequenceList.add(serializedSequence);
+        }
+        jsonGenerator.writeObject(new SerializedSequenceChain<>(serializedSequenceList, serializedBreakList));
     }
 
 }
