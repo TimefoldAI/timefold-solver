08ef069946b344496beab63194f91a3e74e6fd52
Lukáš Petrovický <lukas@petrovicky.net>
2023-12-19 10:49:46 +0100

    ListSwapMoveSelectorTest

--- core/core-impl/src/test/java/ai/timefold/solver/core/impl/heuristic/selector/move/generic/list/ListSwapMoveSelectorTest.java
+++ core/core-impl/src/test/java/ai/timefold/solver/core/impl/heuristic/selector/move/generic/list/ListSwapMoveSelectorTest.java
@@ -2,17 +2,19 @@ package ai.timefold.solver.core.impl.heuristic.selector.move.generic.list;
 
 import static ai.timefold.solver.core.impl.heuristic.selector.SelectorTestUtils.solvingStarted;
 import static ai.timefold.solver.core.impl.testdata.domain.list.TestdataListUtils.getListVariableDescriptor;
+import static ai.timefold.solver.core.impl.testdata.domain.list.TestdataListUtils.getPinnedListVariableDescriptor;
 import static ai.timefold.solver.core.impl.testdata.domain.list.TestdataListUtils.mockEntityIndependentValueSelector;
 import static ai.timefold.solver.core.impl.testdata.util.PlannerAssert.assertAllCodesOfMoveSelector;
+import static ai.timefold.solver.core.impl.testdata.util.PlannerAssert.assertAllCodesOfMoveSelectorWithoutSize;
 import static ai.timefold.solver.core.impl.testdata.util.PlannerAssert.assertCodesOfNeverEndingMoveSelector;
 import static ai.timefold.solver.core.impl.testdata.util.PlannerTestUtils.mockScoreDirector;
 
-import ai.timefold.solver.core.api.score.buildin.simple.SimpleScore;
-import ai.timefold.solver.core.impl.domain.variable.descriptor.ListVariableDescriptor;
-import ai.timefold.solver.core.impl.score.director.InnerScoreDirector;
 import ai.timefold.solver.core.impl.testdata.domain.list.TestdataListEntity;
 import ai.timefold.solver.core.impl.testdata.domain.list.TestdataListSolution;
 import ai.timefold.solver.core.impl.testdata.domain.list.TestdataListValue;
+import ai.timefold.solver.core.impl.testdata.domain.list.pinned.index.TestdataPinnedWithIndexListEntity;
+import ai.timefold.solver.core.impl.testdata.domain.list.pinned.index.TestdataPinnedWithIndexListSolution;
+import ai.timefold.solver.core.impl.testdata.domain.list.pinned.index.TestdataPinnedWithIndexListValue;
 
 import org.junit.jupiter.api.Test;
 
@@ -20,19 +22,16 @@ class ListSwapMoveSelectorTest {
 
     @Test
     void original() {
-        TestdataListValue v1 = new TestdataListValue("1");
-        TestdataListValue v2 = new TestdataListValue("2");
-        TestdataListValue v3 = new TestdataListValue("3");
+        var v1 = new TestdataListValue("1");
+        var v2 = new TestdataListValue("2");
+        var v3 = new TestdataListValue("3");
         TestdataListEntity.createWithValues("A", v2, v1);
         TestdataListEntity.createWithValues("B");
         TestdataListEntity.createWithValues("C", v3);
 
-        InnerScoreDirector<TestdataListSolution, SimpleScore> scoreDirector =
-                mockScoreDirector(TestdataListSolution.buildSolutionDescriptor());
-
-        ListVariableDescriptor<TestdataListSolution> listVariableDescriptor = getListVariableDescriptor(scoreDirector);
-
-        ListSwapMoveSelector<TestdataListSolution> moveSelector = new ListSwapMoveSelector<>(
+        var scoreDirector = mockScoreDirector(TestdataListSolution.buildSolutionDescriptor());
+        var listVariableDescriptor = getListVariableDescriptor(scoreDirector);
+        var moveSelector = new ListSwapMoveSelector<>(
                 mockEntityIndependentValueSelector(listVariableDescriptor, v3, v1, v2),
                 mockEntityIndependentValueSelector(listVariableDescriptor, v3, v1, v2),
                 false);
@@ -60,20 +59,52 @@ class ListSwapMoveSelectorTest {
     }
 
     @Test
+    void originalWithPinning() {
+        var v1 = new TestdataPinnedWithIndexListValue("1");
+        var v2 = new TestdataPinnedWithIndexListValue("2");
+        var v3 = new TestdataPinnedWithIndexListValue("3");
+        var a = TestdataPinnedWithIndexListEntity.createWithValues("A", v2, v1);
+        a.setPlanningPinToIndex(1); // Ignore v2
+        var b = TestdataPinnedWithIndexListEntity.createWithValues("B");
+        b.setPinned(true); // Ignore entirely.
+        TestdataPinnedWithIndexListEntity.createWithValues("C", v3);
+
+        var scoreDirector = mockScoreDirector(TestdataPinnedWithIndexListSolution.buildSolutionDescriptor());
+        var listVariableDescriptor = getPinnedListVariableDescriptor(scoreDirector);
+        var moveSelector = new ListSwapMoveSelector<>(
+                mockEntityIndependentValueSelector(listVariableDescriptor, v3, v1, v2),
+                mockEntityIndependentValueSelector(listVariableDescriptor, v3, v1, v2),
+                false);
+
+        solvingStarted(moveSelector, scoreDirector);
+
+        // Value order: [3, 1, 2]
+        // Entity order: [A, B, C]
+        // Initial state:
+        // - A [2, 1]
+        // - B []
+        // - C [3]
+
+        assertAllCodesOfMoveSelectorWithoutSize(moveSelector,
+                "3 {C[0]} <-> 3 {C[0]}", // undoable
+                "3 {C[0]} <-> 1 {A[1]}",
+                "1 {A[1]} <-> 3 {C[0]}", // redundant
+                "1 {A[1]} <-> 1 {A[1]}" // undoable
+        );
+    }
+
+    @Test
     void random() {
-        TestdataListValue v1 = new TestdataListValue("1");
-        TestdataListValue v2 = new TestdataListValue("2");
-        TestdataListValue v3 = new TestdataListValue("3");
+        var v1 = new TestdataListValue("1");
+        var v2 = new TestdataListValue("2");
+        var v3 = new TestdataListValue("3");
         TestdataListEntity.createWithValues("A", v1, v2);
         TestdataListEntity.createWithValues("B");
         TestdataListEntity.createWithValues("C", v3);
 
-        InnerScoreDirector<TestdataListSolution, SimpleScore> scoreDirector =
-                mockScoreDirector(TestdataListSolution.buildSolutionDescriptor());
-
-        ListVariableDescriptor<TestdataListSolution> listVariableDescriptor = getListVariableDescriptor(scoreDirector);
-
-        ListSwapMoveSelector<TestdataListSolution> moveSelector = new ListSwapMoveSelector<>(
+        var scoreDirector = mockScoreDirector(TestdataListSolution.buildSolutionDescriptor());
+        var listVariableDescriptor = getListVariableDescriptor(scoreDirector);
+        var moveSelector = new ListSwapMoveSelector<>(
                 // Value selectors are longer than the number of expected codes because they're expected
                 // to be never ending, so they must not be exhausted after the last asserted code.
                 mockEntityIndependentValueSelector(listVariableDescriptor, v2, v3, v2, v3, v2, v3, v1, v1, v1, v1),
@@ -92,5 +123,35 @@ class ListSwapMoveSelectorTest {
                 "1 {A[0]} <-> 1 {A[0]}",
                 "1 {A[0]} <-> 2 {A[1]}",
                 "1 {A[0]} <-> 3 {C[0]}");
+    }
+
+    @Test
+    void randomWithPinning() {
+        var v1 = new TestdataPinnedWithIndexListValue("1");
+        var v2 = new TestdataPinnedWithIndexListValue("2");
+        var v3 = new TestdataPinnedWithIndexListValue("3");
+        var a = TestdataPinnedWithIndexListEntity.createWithValues("A", v1, v2);
+        a.setPlanningPinToIndex(1); // Ignore v1
+        var b = TestdataPinnedWithIndexListEntity.createWithValues("B");
+        b.setPinned(true); // Ignore entirely.
+        TestdataPinnedWithIndexListEntity.createWithValues("C", v3);
+
+        var scoreDirector = mockScoreDirector(TestdataPinnedWithIndexListSolution.buildSolutionDescriptor());
+        var listVariableDescriptor = getPinnedListVariableDescriptor(scoreDirector);
+        var moveSelector = new ListSwapMoveSelector<>(
+                // Value selectors are longer than the number of expected codes because they're expected
+                // to be never ending, so they must not be exhausted after the last asserted code.
+                mockEntityIndependentValueSelector(listVariableDescriptor, v1, v2, v3, v1, v2, v3, v1, v2, v3),
+                mockEntityIndependentValueSelector(listVariableDescriptor, v1, v3, v2, v1, v3, v2, v1, v3, v2),
+                true);
+
+        solvingStarted(moveSelector, scoreDirector);
+
+        assertCodesOfNeverEndingMoveSelector(moveSelector,
+                "2 {A[1]} <-> 3 {C[0]}",
+                "3 {C[0]} <-> 2 {A[1]}",
+                "2 {A[1]} <-> 3 {C[0]}",
+                "3 {C[0]} <-> 2 {A[1]}",
+                "2 {A[1]} <-> 3 {C[0]}");
     }
 }
