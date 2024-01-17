922fde922beb2cbc59bee3a90382ffab3fa53eff
zepfred <zepfred@gmail.com>
2024-01-17 12:31:37 -0300

    fix: improve the output of DevUI components

--- quarkus-integration/quarkus/deployment/src/main/resources/dev-ui/constraints-component.js
+++ quarkus-integration/quarkus/deployment/src/main/resources/dev-ui/constraints-component.js
@@ -13,14 +13,14 @@ export class ConstraintsComponent extends LitElement {
 
     // Component style
     static styles = css`
-      .button {
-        background-color: transparent;
-        cursor: pointer;
-      }
+        .button {
+            background-color: transparent;
+            cursor: pointer;
+        }
 
-      .clearIcon {
-        color: orange;
-      }
+        .clearIcon {
+            color: orange;
+        }
     `;
 
     // Component properties
@@ -36,10 +36,7 @@ export class ConstraintsComponent extends LitElement {
     connectedCallback() {
         super.connectedCallback();
         this.jsonRpc.getConstraints().then(jsonRpcResponse => {
-            this._constraints = [];
-            jsonRpcResponse.result.forEach(c => {
-                this._constraints.push(c);
-            });
+            this._constraints = jsonRpcResponse.result;
         });
     }
 
@@ -56,19 +53,52 @@ export class ConstraintsComponent extends LitElement {
     _renderConstraintTable() {
         if (this._constraints) {
             let constraints = this._constraints;
-            return html`
-                <vaadin-grid .items="${constraints}" class="datatable" theme="no-border">
-                    <vaadin-grid-column auto-width
-                                        header="Name"
-                                        ${columnBodyRenderer(this._nameRenderer, [])}>
-                    </vaadin-grid-column>
-                </vaadin-grid>`;
+            const keys = Object.keys(constraints);
+            if (keys.length === 1) {
+                return html`
+                    <vaadin-grid .items="${constraints[keys[0]]}" class="datatable" theme="no-border">
+                        <vaadin-grid-column auto-width
+                                            header="Constraint"
+                                            ${columnBodyRenderer(this._nameRenderer, [])}>
+                        </vaadin-grid-column>
+                    </vaadin-grid>`;
+            } else {
+                let constraintValues = [];
+                keys.sort().forEach(k => {
+                    constraints[k].forEach(v => {
+                        constraintValues.push({
+                            name: k, value: v
+                        });
+                    });
+                });
+                return html`
+                    <vaadin-grid .items="${constraintValues}" class="datatable" theme="no-border" all-rows-visible>
+                        <vaadin-grid-column auto-width
+                                            header="Name"
+                                            ${columnBodyRenderer(this._constraintNameRenderer, [])}>
+                        </vaadin-grid-column>
+                        <vaadin-grid-column auto-width
+                                            header="Constraint"
+                                            ${columnBodyRenderer(this._constraintValueRenderer, [])}>
+                        </vaadin-grid-column>
+                    </vaadin-grid>`;
+            }
         }
     }
 
     _nameRenderer(constraint) {
         return html`
             ${constraint}`;
+    }
+
+    _constraintNameRenderer(constraint) {
+        return html`
+            ${constraint['name']}`;
+    }
+
+    _constraintValueRenderer(constraint) {
+        return html`
+            ${constraint['value']}`;
     }
 
 }
--- quarkus-integration/quarkus/deployment/src/main/resources/dev-ui/model-component.js
+++ quarkus-integration/quarkus/deployment/src/main/resources/dev-ui/model-component.js
@@ -13,14 +13,14 @@ export class ModelComponent extends LitElement {
 
     // Component style
     static styles = css`
-      .button {
-        background-color: transparent;
-        cursor: pointer;
-      }
+        .button {
+            background-color: transparent;
+            cursor: pointer;
+        }
 
-      .clearIcon {
-        color: orange;
-      }
+        .clearIcon {
+            color: orange;
+        }
     `;
 
     // Component properties
@@ -37,14 +37,17 @@ export class ModelComponent extends LitElement {
         super.connectedCallback();
         this.jsonRpc.getModelInfo().then(jsonRpcResponse => {
             this._model = {};
-            this._model.solutionClass = jsonRpcResponse.result.solutionClass;
-            this._model.entityInfoList = [];
-            jsonRpcResponse.result.entityClassList.forEach(entityClass => {
-                const entityInfo = {};
-                entityInfo.name = entityClass;
-                entityInfo.genuineVariableList = jsonRpcResponse.result.entityClassToGenuineVariableListMap[entityClass];
-                entityInfo.shadowVariableList = jsonRpcResponse.result.entityClassToShadowVariableListMap[entityClass];
-                this._model.entityInfoList.push(entityInfo);
+            Object.keys(jsonRpcResponse.result).forEach(key => {
+                this._model[key] = {};
+                this._model[key].solutionClass = jsonRpcResponse.result[key].solutionClass;
+                this._model[key].entityInfoList = [];
+                jsonRpcResponse.result[key].entityClassList.forEach(entityClass => {
+                    const entityInfo = {};
+                    entityInfo.name = entityClass;
+                    entityInfo.genuineVariableList = jsonRpcResponse.result[key].entityClassToGenuineVariableListMap[entityClass];
+                    entityInfo.shadowVariableList = jsonRpcResponse.result[key].entityClassToShadowVariableListMap[entityClass];
+                    this._model[key].entityInfoList.push(entityInfo);
+                });
             });
         });
     }
@@ -62,26 +65,90 @@ export class ModelComponent extends LitElement {
     _renderModel() {
         if (this._model) {
             let model = this._model;
-            return html`
-                <div>
-                    <span>Solution Class:</span>
-                    <span>${model.solutionClass}</span>
-                </div>
-                <vaadin-grid .items="${model.entityInfoList}" class="datatable" theme="no-border">
-                    <vaadin-grid-column auto-width
-                                        header="Entity Class"
-                                        ${columnBodyRenderer(this._entityClassNameRenderer, [])}>
-                    </vaadin-grid-column>
-                    <vaadin-grid-column auto-width
-                                        header="Genuine Variables"
-                                        ${columnBodyRenderer(this._genuineVariablesRenderer, [])}>
-                    </vaadin-grid-column>
-                    <vaadin-grid-column auto-width
-                                        header="Shadow Variables"
-                                        ${columnBodyRenderer(this._shadowVariablesRenderer, [])}>
-                    </vaadin-grid-column>
-                </vaadin-grid>`;
+            const keys = Object.keys(model);
+            if (keys.length === 1) {
+                return html`
+                    <div>
+                        <span>Solution Class:</span>
+                        <span>${model[keys[0]].solutionClass}</span>
+                    </div>
+                    <vaadin-grid .items="${model[keys[0]].entityInfoList}" class="datatable" theme="no-border">
+                        <vaadin-grid-column auto-width
+                                            header="Entity Class"
+                                            ${columnBodyRenderer(this._entityClassNameRenderer, [])}>
+                        </vaadin-grid-column>
+                        <vaadin-grid-column auto-width
+                                            header="Genuine Variables"
+                                            ${columnBodyRenderer(this._genuineVariablesRenderer, [])}>
+                        </vaadin-grid-column>
+                        <vaadin-grid-column auto-width
+                                            header="Shadow Variables"
+                                            ${columnBodyRenderer(this._shadowVariablesRenderer, [])}>
+                        </vaadin-grid-column>
+                    </vaadin-grid>`;
+            } else {
+                let modelValues = [];
+                keys.sort().forEach(k => {
+                    model[k].entityInfoList.forEach(e => {
+                        modelValues.push({
+                            name: k,
+                            solutionClass: model[k].solutionClass,
+                            entityClassName: e.name,
+                            genuineVariableList: e.genuineVariableList,
+                            shadowVariableList: e.shadowVariableList,
+                        });
+                    });
+                });
+                return html`
+                    <vaadin-grid .items="${modelValues}" class="datatable" theme="no-border" all-rows-visible>
+                        <vaadin-grid-column auto-width
+                                            header="Name"
+                                            ${columnBodyRenderer(this._nameRenderer, [])}>
+                        </vaadin-grid-column>
+                        <vaadin-grid-column auto-width
+                                            header="Solution Class"
+                                            ${columnBodyRenderer(this._solutionClassNameRenderer, [])}>
+                        </vaadin-grid-column>
+                        <vaadin-grid-column auto-width
+                                            header="Entity Class"
+                                            ${columnBodyRenderer(this._cassNameRendererPerEntry, [])}>
+                        </vaadin-grid-column>
+                        <vaadin-grid-column auto-width
+                                            header="Genuine Variables"
+                                            ${columnBodyRenderer(this._genuineVariablesRendererPerEntry, [])}>
+                        </vaadin-grid-column>
+                        <vaadin-grid-column auto-width
+                                            header="Shadow Variables"
+                                            ${columnBodyRenderer(this._shadowVariablesRendererPerEntry, [])}>
+                        </vaadin-grid-column>
+                    </vaadin-grid>`;
+            }
         }
+    }
+
+    _nameRenderer(entry) {
+        return html`
+            ${entry.name}`;
+    }
+
+    _solutionClassNameRenderer(entry) {
+        return html`
+            ${entry.solutionClass}`;
+    }
+
+    _cassNameRendererPerEntry(entry) {
+        return html`
+            ${entry.entityClassName}`;
+    }
+
+    _genuineVariablesRendererPerEntry(entry) {
+        return html`
+            ${entry.genuineVariableList.join(', ')}`;
+    }
+
+    _shadowVariablesRendererPerEntry(entry) {
+        return html`
+            ${entry.shadowVariableList.join(', ')}`;
     }
 
     _entityClassNameRenderer(entityInfo) {
