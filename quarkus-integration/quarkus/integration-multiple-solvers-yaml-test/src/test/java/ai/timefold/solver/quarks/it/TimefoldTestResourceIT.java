8e07a5629a64e104cef50fb9a01eec7e5afcd4f8
Fred <zepfred@gmail.com>
2024-02-14 08:35:27 -0300

    test: Quarks YAML tests

---
+++ quarkus-integration/quarkus/integration-multiple-solvers-yaml-test/src/test/java/ai/timefold/solver/quarks/it/TimefoldTestResourceIT.java
@@ -0,0 +1,11 @@
+package ai.timefold.solver.quarks.it;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+/**
+ * Test various Timefold operations running in native mode
+ */
+@QuarkusIntegrationTest
+public class TimefoldTestResourceIT extends TimefoldTestResourceTest {
+
+}
---
+++ quarkus-integration/quarkus/integration-yaml-test/src/main/java/ai/timefold/solver/quarkus/it/domain/TestdataStringLengthShadowSolution.java
@@ -0,0 +1,49 @@
+package ai.timefold.solver.quarkus.it.domain;
+
+import java.util.List;
+
+import ai.timefold.solver.core.api.domain.solution.PlanningEntityCollectionProperty;
+import ai.timefold.solver.core.api.domain.solution.PlanningScore;
+import ai.timefold.solver.core.api.domain.solution.PlanningSolution;
+import ai.timefold.solver.core.api.domain.valuerange.ValueRangeProvider;
+import ai.timefold.solver.core.api.score.buildin.hardsoft.HardSoftScore;
+
+@PlanningSolution
+public class TestdataStringLengthShadowSolution {
+
+    @ValueRangeProvider(id = "valueRange")
+    private List<String> valueList;
+    @PlanningEntityCollectionProperty
+    private List<TestdataStringLengthShadowEntity> entityList;
+
+    @PlanningScore
+    private HardSoftScore score;
+
+    // ************************************************************************
+    // Getters/setters
+    // ************************************************************************
+
+    public List<String> getValueList() {
+        return valueList;
+    }
+
+    public void setValueList(List<String> valueList) {
+        this.valueList = valueList;
+    }
+
+    public List<TestdataStringLengthShadowEntity> getEntityList() {
+        return entityList;
+    }
+
+    public void setEntityList(List<TestdataStringLengthShadowEntity> entityList) {
+        this.entityList = entityList;
+    }
+
+    public HardSoftScore getScore() {
+        return score;
+    }
+
+    public void setScore(HardSoftScore score) {
+        this.score = score;
+    }
+}
