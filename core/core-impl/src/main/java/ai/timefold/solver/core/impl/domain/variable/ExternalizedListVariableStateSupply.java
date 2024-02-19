e99bc6c535876e45b6393591701fc53cad5297c3
Lukáš Petrovický <lukas@petrovicky.net>
2024-02-19 18:52:13 +0100

    chore: prematurely terminate updates

--- core/core-impl/src/main/java/ai/timefold/solver/core/impl/domain/variable/ExternalizedListVariableStateSupply.java
+++ core/core-impl/src/main/java/ai/timefold/solver/core/impl/domain/variable/ExternalizedListVariableStateSupply.java
@@ -13,7 +13,7 @@ final class ExternalizedListVariableStateSupply<Solution_>
         implements ListVariableStateSupply<Solution_> {
 
     private final ListVariableDescriptor<Solution_> sourceVariableDescriptor;
-    private Map<Object, ElementLocation> elementLocationMap;
+    private Map<Object, LocationInList> elementLocationMap;
     private int unassignedCount;
 
     public ExternalizedListVariableStateSupply(ListVariableDescriptor<Solution_> sourceVariableDescriptor) {
@@ -22,8 +22,12 @@ final class ExternalizedListVariableStateSupply<Solution_>
 
     @Override
     public void resetWorkingSolution(ScoreDirector<Solution_> scoreDirector) {
-        this.elementLocationMap = new IdentityHashMap<>();
         var workingSolution = scoreDirector.getWorkingSolution();
+        if (elementLocationMap == null) {
+            elementLocationMap = new IdentityHashMap<>((int) sourceVariableDescriptor.getValueRangeSize(workingSolution, null));
+        } else {
+            elementLocationMap.clear();
+        }
         // Start with everything unassigned.
         unassignedCount = (int) sourceVariableDescriptor.getValueRangeSize(workingSolution, null);
         // Will run over all entities and unmark all present elements as unassigned.
@@ -77,17 +81,16 @@ final class ExternalizedListVariableStateSupply<Solution_>
         for (var index = 0; index < assignedElements.size(); index++) {
             var element = assignedElements.get(index);
             var oldElementLocation = elementLocationMap.remove(element);
-            if (oldElementLocation instanceof LocationInList oldLocationInlist) {
-                var oldIndex = oldLocationInlist.index();
-                if (oldIndex != index) {
-                    throw new IllegalStateException(
-                            "The supply (%s) is corrupted, because the element (%s) at index (%d) had an old index (%d) which is not the current index (%d)."
-                                    .formatted(this, element, index, oldIndex, index));
-                }
-            } else {
+            if (oldElementLocation == null) {
                 throw new IllegalStateException(
                         "The supply (%s) is corrupted, because the element (%s) at index (%d) was already unassigned (%s)."
                                 .formatted(this, element, index, oldElementLocation));
+            }
+            var oldIndex = oldElementLocation.index();
+            if (oldIndex != index) {
+                throw new IllegalStateException(
+                        "The supply (%s) is corrupted, because the element (%s) at index (%d) had an old index (%d) which is not the current index (%d)."
+                                .formatted(this, element, index, oldIndex, index));
             }
             unassignedCount++;
         }
@@ -111,17 +114,22 @@ final class ExternalizedListVariableStateSupply<Solution_>
 
     @Override
     public void afterListVariableChanged(ScoreDirector<Solution_> scoreDirector, Object o, int fromIndex, int toIndex) {
-        updateIndexes(o, fromIndex);
+        updateIndexes(o, fromIndex, toIndex);
     }
 
-    private void updateIndexes(Object entity, int startIndex) {
+    private void updateIndexes(Object entity, int startIndex, int toIndex) {
         var assignedElements = sourceVariableDescriptor.getValue(entity);
         for (var index = startIndex; index < assignedElements.size(); index++) {
             var element = assignedElements.get(index);
-            var oldLocation = elementLocationMap.put(element, new LocationInList(entity, index));
-            if (!(oldLocation instanceof LocationInList)) {
-                // Fixes the lack of before/afterListVariableElementAssigned().
+            var newLocation = new LocationInList(entity, index);
+            var oldLocation = elementLocationMap.put(element, newLocation);
+            if (oldLocation == null) {
                 unassignedCount--;
+            } else if (index >= toIndex && newLocation.equals(oldLocation)) {
+                // Location is unchanged and we are past the part of the list that changed.
+                return;
+            } else {
+                // Continue to the next element.
             }
         }
     }
@@ -138,7 +146,7 @@ final class ExternalizedListVariableStateSupply<Solution_>
         if (elementLocation == null) {
             return null;
         }
-        return elementLocation instanceof LocationInList elementLocationInList ? elementLocationInList.index() : null;
+        return elementLocation.index();
     }
 
     @Override
@@ -147,7 +155,7 @@ final class ExternalizedListVariableStateSupply<Solution_>
         if (elementLocation == null) {
             return null;
         }
-        return elementLocation instanceof LocationInList elementLocationInList ? elementLocationInList.entity() : null;
+        return elementLocation.entity();
     }
 
     @Override
