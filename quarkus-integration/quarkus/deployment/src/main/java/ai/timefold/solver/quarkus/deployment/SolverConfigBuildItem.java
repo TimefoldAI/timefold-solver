f05fa79ee13ce9d4dac27c52ace19c7103ce2c7e
Christopher Chianelli <christopher@timefold.ai>
2024-02-21 15:39:39 -0500

    chore: Add enterprise docs for node sharing, fix method name typo

--- quarkus-integration/quarkus/deployment/src/main/java/ai/timefold/solver/quarkus/deployment/SolverConfigBuildItem.java
+++ quarkus-integration/quarkus/deployment/src/main/java/ai/timefold/solver/quarkus/deployment/SolverConfigBuildItem.java
@@ -1,11 +1,8 @@
 package ai.timefold.solver.quarkus.deployment;
 
 import java.util.Map;
-import java.util.Objects;
-import java.util.Set;
 
 import ai.timefold.solver.core.config.solver.SolverConfig;
-import ai.timefold.solver.quarkus.deployment.config.TimefoldBuildTimeConfig;
 
 import io.quarkus.builder.item.SimpleBuildItem;
 
@@ -21,28 +18,7 @@ public final class SolverConfigBuildItem extends SimpleBuildItem {
         this.generatedGizmoClasses = generatedGizmoClasses;
     }
 
-    /**
-     * Returns the configuration of a given solver name.
-     *
-     * @param solverName never null, the solver name
-     */
-    public SolverConfig getSolverConfig(String solverName) {
-        return this.solverConfigurations
-                .get(Objects.requireNonNull(solverName, "Invalid solverName (null) given to SolverConfigBuildItem."));
-    }
-
-    /**
-     * Returns the configuration of the default solver.
-     */
-    public SolverConfig getSolverConfig() {
-        return this.solverConfigurations.get(TimefoldBuildTimeConfig.DEFAULT_SOLVER_NAME);
-    }
-
-    public Set<String> getSolverNames() {
-        return this.solverConfigurations.keySet();
-    }
-
-    public Map<String, SolverConfig> getSolvetConfigMap() {
+    public Map<String, SolverConfig> getSolverConfigMap() {
         return solverConfigurations;
     }
 
