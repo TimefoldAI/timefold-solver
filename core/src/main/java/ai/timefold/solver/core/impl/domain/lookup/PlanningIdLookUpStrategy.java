21c7dbe518d0000379922cf15eb068cbcf3a3ae2
Lukáš Petrovický <lukas@petrovicky.net>
2024-04-11 12:23:52 +0200

    Sonar

--- core/src/main/java/ai/timefold/solver/core/impl/domain/lookup/PlanningIdLookUpStrategy.java
+++ core/src/main/java/ai/timefold/solver/core/impl/domain/lookup/PlanningIdLookUpStrategy.java
@@ -19,13 +19,8 @@ public final class PlanningIdLookUpStrategy implements LookUpStrategy {
 
     @Override
     public void addWorkingObject(Map<Object, Object> idToWorkingObjectMap, Object workingObject) {
-        Object planningId = extractPlanningId(workingObject);
-        if (planningId == null) {
-            throw new IllegalStateException("The workingObject (" + workingObject
-                    + ") returns a null value for its @" + PlanningId.class.getSimpleName()
-                    + " member (" + planningIdMemberAccessor + ").");
-        }
-        Object oldAddedObject = idToWorkingObjectMap.put(planningId, workingObject);
+        var planningId = extractPlanningId(workingObject);
+        var oldAddedObject = idToWorkingObjectMap.put(planningId, workingObject);
         if (oldAddedObject != null) {
             throw new IllegalStateException("The workingObjects (" + oldAddedObject + ", " + workingObject
                     + ") have the same planningId (" + planningId + "). Working objects must be unique.");
@@ -34,13 +29,8 @@ public final class PlanningIdLookUpStrategy implements LookUpStrategy {
 
     @Override
     public void removeWorkingObject(Map<Object, Object> idToWorkingObjectMap, Object workingObject) {
-        Object planningId = extractPlanningId(workingObject);
-        if (planningId == null) {
-            throw new IllegalStateException("The workingObject (" + workingObject
-                    + ") returns a null value for its @" + PlanningId.class.getSimpleName()
-                    + " member (" + planningIdMemberAccessor + ").");
-        }
-        Object removedObject = idToWorkingObjectMap.remove(planningId);
+        var planningId = extractPlanningId(workingObject);
+        var removedObject = idToWorkingObjectMap.remove(planningId);
         if (workingObject != removedObject) {
             throw new IllegalStateException("The workingObject (" + workingObject
                     + ") differs from the removedObject (" + removedObject + ") for planningId (" + planningId + ").");
@@ -49,8 +39,8 @@ public final class PlanningIdLookUpStrategy implements LookUpStrategy {
 
     @Override
     public <E> E lookUpWorkingObject(Map<Object, Object> idToWorkingObjectMap, E externalObject) {
-        Object planningId = extractPlanningId(externalObject);
-        E workingObject = (E) idToWorkingObjectMap.get(planningId);
+        var planningId = extractPlanningId(externalObject);
+        var workingObject = (E) idToWorkingObjectMap.get(planningId);
         if (workingObject == null) {
             throw new IllegalStateException("The externalObject (" + externalObject + ") with planningId (" + planningId
                     + ") has no known workingObject (" + workingObject + ").\n"
@@ -64,12 +54,12 @@ public final class PlanningIdLookUpStrategy implements LookUpStrategy {
 
     @Override
     public <E> E lookUpWorkingObjectIfExists(Map<Object, Object> idToWorkingObjectMap, E externalObject) {
-        Object planningId = extractPlanningId(externalObject);
+        var planningId = extractPlanningId(externalObject);
         return (E) idToWorkingObjectMap.get(planningId);
     }
 
-    protected Object extractPlanningId(Object externalObject) {
-        Object planningId = planningIdMemberAccessor.executeGetter(externalObject);
+    private Pair<Class<?>, Object> extractPlanningId(Object externalObject) {
+        var planningId = planningIdMemberAccessor.executeGetter(externalObject);
         if (planningId == null) {
             throw new IllegalArgumentException("The planningId (" + planningId
                     + ") of the member (" + planningIdMemberAccessor + ") of the class (" + externalObject.getClass()
