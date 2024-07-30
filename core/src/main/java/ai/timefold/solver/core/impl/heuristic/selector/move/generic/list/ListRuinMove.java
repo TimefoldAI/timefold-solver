23b8a7c425009f6877fb799a7f7cedb321f3c885
Christopher Chianelli <christopher@timefold.ai>
2024-07-30 12:57:48 -0400

    fix: Call variable listeners after each removal to update positions

--- core/src/main/java/ai/timefold/solver/core/impl/heuristic/selector/move/generic/list/ListRuinMove.java
+++ core/src/main/java/ai/timefold/solver/core/impl/heuristic/selector/move/generic/list/ListRuinMove.java
@@ -1,11 +1,5 @@
 package ai.timefold.solver.core.impl.heuristic.selector.move.generic.list;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
-
 import ai.timefold.solver.core.api.score.director.ScoreDirector;
 import ai.timefold.solver.core.impl.constructionheuristic.DefaultConstructionHeuristicPhase;
 import ai.timefold.solver.core.impl.domain.variable.ListVariableStateSupply;
@@ -15,6 +9,12 @@ import ai.timefold.solver.core.impl.heuristic.move.Move;
 import ai.timefold.solver.core.impl.heuristic.selector.list.LocationInList;
 import ai.timefold.solver.core.impl.score.director.VariableDescriptorAwareScoreDirector;
 import ai.timefold.solver.core.impl.solver.scope.SolverScope;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class ListRuinMove<Solution_> extends AbstractMove<Solution_> {
     protected final Object[] ruinedValues;
@@ -96,9 +96,9 @@ public class ListRuinMove<Solution_> extends AbstractMove<Solution_> {
                 innerScoreDirector.afterListVariableElementUnassigned(listVariableDescriptor, ruinedValue);
                 innerScoreDirector.afterListVariableChanged(listVariableDescriptor, oldLocation.entity(), oldLocation.index(),
                         oldLocation.index());
+                scoreDirector.triggerVariableListeners();
             }
 
-            scoreDirector.triggerVariableListeners();
             constructionHeuristicPhase.solvingStarted(solverScope);
             constructionHeuristicPhase.solve(solverScope);
             scoreDirector.triggerVariableListeners();
