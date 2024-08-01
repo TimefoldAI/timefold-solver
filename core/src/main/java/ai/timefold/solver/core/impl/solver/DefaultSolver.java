8db5a4b1b7cb9f3ed15e224b33f70c362e46030c
Christopher Chianelli <christopher@timefold.ai>
2024-08-01 22:35:07 +0200

    chore: ListRuinMove refactor
    - All versions of this move (including previous) currently have
      shadow variable corruption

--- core/src/main/java/ai/timefold/solver/core/impl/solver/DefaultSolver.java
+++ core/src/main/java/ai/timefold/solver/core/impl/solver/DefaultSolver.java
@@ -89,17 +89,7 @@ public class DefaultSolver<Solution_> extends AbstractSolver<Solution_> {
     // ************************************************************************
 
     public long getTimeMillisSpent() {
-        Long startingSystemTimeMillis = solverScope.getStartingSystemTimeMillis();
-        if (startingSystemTimeMillis == null) {
-            // The solver hasn't started yet
-            return 0L;
-        }
-        Long endingSystemTimeMillis = solverScope.getEndingSystemTimeMillis();
-        if (endingSystemTimeMillis == null) {
-            // The solver hasn't ended yet
-            endingSystemTimeMillis = System.currentTimeMillis();
-        }
-        return endingSystemTimeMillis - startingSystemTimeMillis;
+        return solverScope.getTimeMillisSpent();
     }
 
     public long getScoreCalculationCount() {
@@ -107,7 +97,7 @@ public class DefaultSolver<Solution_> extends AbstractSolver<Solution_> {
     }
 
     public long getScoreCalculationSpeed() {
-        return SolverScope.getScoreCalculationSpeed(getScoreCalculationCount(), getTimeMillisSpent());
+        return solverScope.getScoreCalculationSpeed();
     }
 
     @Override
--- core/src/test/java/ai/timefold/solver/core/impl/heuristic/selector/move/generic/list/ListRuinMoveSelectorTest.java
+++ core/src/test/java/ai/timefold/solver/core/impl/heuristic/selector/move/generic/list/ListRuinMoveSelectorTest.java
@@ -1,7 +1,6 @@
 package ai.timefold.solver.core.impl.heuristic.selector.move.generic.list;
 
-import java.util.List;
-
+import ai.timefold.solver.core.api.score.buildin.simple.SimpleScore;
 import ai.timefold.solver.core.api.score.stream.Constraint;
 import ai.timefold.solver.core.api.score.stream.ConstraintFactory;
 import ai.timefold.solver.core.api.score.stream.ConstraintProvider;
@@ -15,8 +14,10 @@ import ai.timefold.solver.core.config.solver.termination.TerminationConfig;
 import ai.timefold.solver.core.impl.testdata.domain.list.TestdataListEntity;
 import ai.timefold.solver.core.impl.testdata.domain.list.TestdataListSolution;
 import ai.timefold.solver.core.impl.testdata.domain.list.TestdataListValue;
-
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Objects;
 
 class ListRuinMoveSelectorTest {
     public static class TestdataListConstraintProvider implements ConstraintProvider {
@@ -24,6 +25,12 @@ class ListRuinMoveSelectorTest {
         @Override
         public Constraint[] defineConstraints(ConstraintFactory constraintFactory) {
             return new Constraint[] {
+                    constraintFactory.forEach(TestdataListValue.class)
+                            .penalize(SimpleScore.ONE, value -> Math.abs(
+                                    Objects.hash(value.getEntity().getCode().hashCode(),
+                                            value.getIndex(),
+                                            value.getCode())) >> 8)
+                            .asConstraint("Hash constraint")
             };
         }
     }
@@ -41,7 +48,7 @@ class ListRuinMoveSelectorTest {
                                 .withMoveSelectorConfig(
                                         new ListRuinMoveSelectorConfig())
                                 .withTerminationConfig(new TerminationConfig()
-                                        .withStepCountLimit(20))));
+                                        .withStepCountLimit(30))));
         var problem = TestdataListSolution.generateUninitializedSolution(30, 30);
         var solver = SolverFactory.create(solverConfig).buildSolver();
         solver.solve(problem);
