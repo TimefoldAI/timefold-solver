0093727669a3b509e5b57ce12905820ecb35a49e
Fred <zepfred@gmail.com>
2024-09-04 14:41:19 -0300

    fix: address quarkus upgrade issues

--- quarkus-integration/quarkus-benchmark/deployment/src/main/java/ai/timefold/solver/benchmark/quarkus/deployment/TimefoldBenchmarkBuildTimeConfig.java
+++ quarkus-integration/quarkus-benchmark/deployment/src/main/java/ai/timefold/solver/benchmark/quarkus/deployment/TimefoldBenchmarkBuildTimeConfig.java
@@ -2,21 +2,23 @@ package ai.timefold.solver.benchmark.quarkus.deployment;
 
 import java.util.Optional;
 
-import io.quarkus.runtime.annotations.ConfigItem;
+import io.quarkus.runtime.annotations.ConfigPhase;
 import io.quarkus.runtime.annotations.ConfigRoot;
+import io.smallrye.config.ConfigMapping;
 
 /**
  * During build time, this is translated into Timefold's Config classes.
  */
-@ConfigRoot(name = "timefold.benchmark")
-public class TimefoldBenchmarkBuildTimeConfig {
+@ConfigMapping(prefix = "quarkus.timefold.benchmark")
+@ConfigRoot(phase = ConfigPhase.BUILD_TIME)
+public interface TimefoldBenchmarkBuildTimeConfig {
 
-    public static final String DEFAULT_SOLVER_BENCHMARK_CONFIG_URL = "solverBenchmarkConfig.xml";
+    String DEFAULT_SOLVER_BENCHMARK_CONFIG_URL = "solverBenchmarkConfig.xml";
+
     /**
      * A classpath resource to read the benchmark configuration XML.
      * Defaults to {@value DEFAULT_SOLVER_BENCHMARK_CONFIG_URL}.
      * If this property isn't specified, that solverBenchmarkConfig.xml is optional.
      */
-    @ConfigItem
-    Optional<String> solverBenchmarkConfigXml;
+    Optional<String> solverBenchmarkConfigXml();
 }
