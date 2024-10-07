8d5bb66d66daccf1e5f2361271fa8fa1fc2d8b59
Lukáš Petrovický <lukas@petrovicky.net>
2024-10-07 13:01:43 +0200

    Final fix

---
+++ core/src/main/java/ai/timefold/solver/core/impl/solver/DefaultRecommendedFit.java
@@ -0,0 +1,27 @@
+package ai.timefold.solver.core.impl.solver;
+
+import ai.timefold.solver.core.api.score.Score;
+import ai.timefold.solver.core.api.score.analysis.ScoreAnalysis;
+import ai.timefold.solver.core.api.solver.RecommendedFit;
+
+/**
+ * @deprecated Prefer {@link DefaultRecommendedAssignment} instead.
+ */
+@Deprecated(forRemoval = true, since = "1.15.0")
+public record DefaultRecommendedFit<Proposition_, Score_ extends Score<Score_>>(long index, Proposition_ proposition,
+        ScoreAnalysis<Score_> scoreAnalysisDiff)
+        implements
+            RecommendedFit<Proposition_, Score_>,
+            Comparable<DefaultRecommendedFit<Proposition_, Score_>> {
+
+    @Override
+    public int compareTo(DefaultRecommendedFit<Proposition_, Score_> other) {
+        int scoreComparison = scoreAnalysisDiff.score().compareTo(other.scoreAnalysisDiff.score());
+        if (scoreComparison != 0) {
+            return -scoreComparison; // Better scores first.
+        }
+        // Otherwise maintain insertion order.
+        return Long.compareUnsigned(index, other.index); // Unsigned == many more positive values.
+    }
+
+}
