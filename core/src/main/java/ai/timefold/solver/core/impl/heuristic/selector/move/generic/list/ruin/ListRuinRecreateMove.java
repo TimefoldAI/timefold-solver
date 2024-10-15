68381087d8ce722a895e46d4e4e905c28d110022
Christopher Chianelli <christopher@timefold.ai>
2024-10-15 15:57:16 -0400

    chore: WIP Simplified RnR List Move

--- core/src/main/java/ai/timefold/solver/core/impl/heuristic/selector/move/generic/list/ruin/ListRuinRecreateMove.java
+++ core/src/main/java/ai/timefold/solver/core/impl/heuristic/selector/move/generic/list/ruin/ListRuinRecreateMove.java
@@ -1,5 +1,6 @@
 package ai.timefold.solver.core.impl.heuristic.selector.move.generic.list.ruin;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
@@ -11,13 +12,14 @@ import java.util.TreeSet;
 import ai.timefold.solver.core.api.score.director.ScoreDirector;
 import ai.timefold.solver.core.impl.domain.variable.ListVariableStateSupply;
 import ai.timefold.solver.core.impl.heuristic.move.AbstractMove;
+import ai.timefold.solver.core.impl.heuristic.move.AbstractSimplifiedMove;
 import ai.timefold.solver.core.impl.heuristic.move.Move;
 import ai.timefold.solver.core.impl.heuristic.selector.move.generic.RuinRecreateConstructionHeuristicPhaseBuilder;
-import ai.timefold.solver.core.impl.score.director.VariableDescriptorAwareScoreDirector;
+import ai.timefold.solver.core.impl.move.director.VariableChangeRecordingScoreDirector;
 import ai.timefold.solver.core.impl.solver.scope.SolverScope;
 import ai.timefold.solver.core.impl.util.CollectionUtils;
 
-public final class ListRuinRecreateMove<Solution_> extends AbstractMove<Solution_> {
+public final class ListRuinRecreateMove<Solution_> extends AbstractSimplifiedMove<Solution_> {
 
     private final ListVariableStateSupply<Solution_> listVariableStateSupply;
     private final List<Object> ruinedValueList;
@@ -40,17 +42,11 @@ public final class ListRuinRecreateMove<Solution_> extends AbstractMove<Solution
     }
 
     @Override
-    protected Move<Solution_> createUndoMove(ScoreDirector<Solution_> scoreDirector) {
-        return new ListRuinRecreateUndoMove<>(this, listVariableStateSupply.getSourceVariableDescriptor(),
-                entityToNewPositionMap, entityToOriginalPositionMap);
-    }
-
-    @Override
     protected void doMoveOnGenuineVariables(ScoreDirector<Solution_> scoreDirector) {
         entityToNewPositionMap.clear();
         entityToOriginalPositionMap.clear();
 
-        var innerScoreDirector = (VariableDescriptorAwareScoreDirector<Solution_>) scoreDirector;
+        var innerScoreDirector = (VariableChangeRecordingScoreDirector<Solution_>) scoreDirector;
         for (var value : ruinedValueList) {
             var location = listVariableStateSupply.getLocationInList(value)
                     .ensureAssigned();
@@ -59,18 +55,21 @@ public final class ListRuinRecreateMove<Solution_> extends AbstractMove<Solution
         }
 
         var listVariableDescriptor = listVariableStateSupply.getSourceVariableDescriptor();
+
+        var delegateScoreDirector = innerScoreDirector.getDelegate();
         for (var entry : entityToOriginalPositionMap.entrySet()) {
             var entity = entry.getKey();
 
+            // Do only before on recording, so we can restore the state
             innerScoreDirector.beforeListVariableChanged(listVariableDescriptor, entity,
                     listVariableDescriptor.getFirstUnpinnedIndex(entity),
                     listVariableDescriptor.getListSize(entity));
             for (var position : entry.getValue().descendingSet()) {
-                innerScoreDirector.beforeListVariableElementUnassigned(listVariableDescriptor, position.ruinedValue());
+                delegateScoreDirector.beforeListVariableElementUnassigned(listVariableDescriptor, position.ruinedValue());
                 listVariableDescriptor.removeElement(entity, position.index());
-                innerScoreDirector.afterListVariableElementUnassigned(listVariableDescriptor, position.ruinedValue());
+                delegateScoreDirector.afterListVariableElementUnassigned(listVariableDescriptor, position.ruinedValue());
             }
-            innerScoreDirector.afterListVariableChanged(listVariableDescriptor, entity,
+            delegateScoreDirector.afterListVariableChanged(listVariableDescriptor, entity,
                     listVariableDescriptor.getFirstUnpinnedIndex(entity),
                     listVariableDescriptor.getListSize(entity));
         }
@@ -84,12 +83,23 @@ public final class ListRuinRecreateMove<Solution_> extends AbstractMove<Solution
         constructionHeuristicPhase.solvingEnded(solverScope);
         scoreDirector.triggerVariableListeners();
 
+        var entityToInsertedValuesMap = CollectionUtils.<Object, List<Object>> newIdentityHashMap(0);
+        for (var entity : entityToOriginalPositionMap.keySet()) {
+            entityToInsertedValuesMap.put(entity, new ArrayList<>());
+        }
+
         for (var ruinedValue : ruinedValueList) {
             var location = listVariableStateSupply.getLocationInList(ruinedValue)
                     .ensureAssigned();
             entityToNewPositionMap.computeIfAbsent(location.entity(), ignored -> new TreeSet<>())
                     .add(new RuinedLocation(ruinedValue, location.index()));
+            entityToInsertedValuesMap.computeIfAbsent(location.entity(), ignored -> new ArrayList<>()).add(ruinedValue);
         }
+
+        for (var entry : entityToInsertedValuesMap.entrySet()) {
+            innerScoreDirector.recordRuinRecreateListAssignment(listVariableDescriptor, entry.getKey(), entry.getValue());
+        }
+
     }
 
     @Override
@@ -109,8 +119,8 @@ public final class ListRuinRecreateMove<Solution_> extends AbstractMove<Solution
 
     @Override
     public Move<Solution_> rebase(ScoreDirector<Solution_> destinationScoreDirector) {
-        var rebasedRuinedValueList = rebaseList(ruinedValueList, destinationScoreDirector);
-        var rebasedAffectedEntitySet = rebaseSet(affectedEntitySet, destinationScoreDirector);
+        var rebasedRuinedValueList = AbstractMove.rebaseList(ruinedValueList, destinationScoreDirector);
+        var rebasedAffectedEntitySet = AbstractMove.rebaseSet(affectedEntitySet, destinationScoreDirector);
         return new ListRuinRecreateMove<>(listVariableStateSupply, constructionHeuristicPhaseBuilder, solverScope,
                 rebasedRuinedValueList, rebasedAffectedEntitySet);
     }
