536880198d4b2c33cb5f26179ba8ac6c35ee77db
Lukáš Petrovický <lukas@petrovicky.net>
2024-11-07 15:59:44 +0100

    deps: upgrade to Quarkus 3.16.1
    Required us to drop webjars-locator and do the extraction manually, on account of the root issue not being easily and quickly fixable:
    
    https://github.com/classgraph/classgraph/issues/891

--- benchmark/src/main/java/ai/timefold/solver/benchmark/impl/report/WebsiteResourceUtils.java
+++ benchmark/src/main/java/ai/timefold/solver/benchmark/impl/report/WebsiteResourceUtils.java
@@ -1,64 +1,25 @@
 package ai.timefold.solver.benchmark.impl.report;
 
-import static java.util.Collections.emptySet;
-import static java.util.Map.entry;
-import static java.util.Set.of;
-
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
-import java.util.Map;
-import java.util.Set;
-
-import org.webjars.WebJarAssetLocator;
+import java.util.function.Function;
 
 public final class WebsiteResourceUtils {
 
     private static final String RESOURCE_NAMESPACE = "/ai/timefold/solver/benchmark/impl/report/";
+    private static final String WEBJAR_RESOURCE_NAMESPACE = "META-INF/resources/webjars/timefold/";
 
     public static void copyResourcesTo(File benchmarkReportDirectory) {
-        /*
-         * Describe which webjar resources we need.
-         * Otherwise the report would be dozens of megabytes in size.
-         */
-        Map<String, Set<String>> webjarToResourceMap = Map.ofEntries(
-                entry("timefold",
-                        emptySet()),
-                entry("bootstrap",
-                        of("css/bootstrap.min.css", "js/bootstrap.bundle.min.js")),
-                entry("font-awesome",
-                        of("css/all.min.css", "sprites/brands.svg", "sprites/solid.svg")),
-                entry("jquery",
-                        of("jquery.min.js")));
-        // Extract webjars.
-        File webjarDirectory = new File(benchmarkReportDirectory, "website/webjars/");
-        new WebJarAssetLocator()
-                .getAllWebJars()
-                .forEach((artifactId, webjarInfo) -> {
-                    if (!webjarToResourceMap.containsKey(artifactId)) {
-                        return;
-                    }
-                    // The webjarInfo typically contains the version, but we don't want to use it in the path.
-                    String resourcePrefix = "META-INF/resources/webjars/" + artifactId + "/";
-                    String resourcePrefixWithVersion = resourcePrefix + webjarInfo.getVersion() + "/";
-                    File webjarTargetDirectory = new File(webjarDirectory, artifactId);
-                    webjarInfo.getContents().forEach(resource -> {
-                        Set<String> resourceSet = webjarToResourceMap.get(artifactId); // Empty means all resources.
-                        /*
-                         * Some webjars do not have a version in the resource path.
-                         * Case in point: the Timefold webjar.
-                         */
-                        String actualResourcePrefix =
-                                resource.startsWith(resourcePrefixWithVersion) ? resourcePrefixWithVersion : resourcePrefix;
-                        if (resourceSet.isEmpty() || resourceSet.stream().anyMatch(resource::endsWith)) {
-                            // Only copy the resources we need.
-                            String relativePath = resource.substring(actualResourcePrefix.length());
-                            copyResource(webjarTargetDirectory, "/", resource, relativePath);
-                        }
-                    });
-                });
-        // Manually copy some additional resources.
+        // Manually extract webjars, as webjar-locator had issues with Quarkus.
+        copyWebjarResource(benchmarkReportDirectory, "css/timefold-webui.css");
+        copyWebjarResource(benchmarkReportDirectory, "js/timefold-webui.js");
+        copyWebjarResource(benchmarkReportDirectory, "img/timefold-favicon.svg");
+        copyWebjarResource(benchmarkReportDirectory, "img/timefold-logo-horizontal-negative.svg");
+        copyWebjarResource(benchmarkReportDirectory, "img/timefold-logo-horizontal-positive.svg");
+        copyWebjarResource(benchmarkReportDirectory, "img/timefold-logo-stacked-positive.svg");
+        // Manually copy some other resources.
         copyResource(benchmarkReportDirectory, "website/css/prettify.css");
         copyResource(benchmarkReportDirectory, "website/css/app.css");
         copyResource(benchmarkReportDirectory, "website/js/chartjs-plugin-watermark.js");
@@ -66,27 +27,29 @@ public final class WebsiteResourceUtils {
         copyResource(benchmarkReportDirectory, "website/js/app.js");
     }
 
-    private static void copyResource(File benchmarkReportDirectory, String websiteResource) {
-        copyResource(benchmarkReportDirectory, websiteResource, websiteResource);
+    private static void copyWebjarResource(File benchmarkReportDirectory, String websiteResource) {
+        copyResource(benchmarkReportDirectory, WEBJAR_RESOURCE_NAMESPACE, websiteResource, "website/" + websiteResource,
+                s -> WebsiteResourceUtils.class.getClassLoader().getResourceAsStream(s));
     }
 
-    private static void copyResource(File benchmarkReportDirectory, String websiteResource, String targetResource) {
-        copyResource(benchmarkReportDirectory, RESOURCE_NAMESPACE, websiteResource, targetResource);
+    private static void copyResource(File benchmarkReportDirectory, String websiteResource) {
+        copyResource(benchmarkReportDirectory, RESOURCE_NAMESPACE, websiteResource, websiteResource,
+                WebsiteResourceUtils.class::getResourceAsStream);
     }
 
     private static void copyResource(File benchmarkReportDirectory, String namespace, String websiteResource,
-            String targetResource) {
-        File outputFile = new File(benchmarkReportDirectory, targetResource);
+            String targetResource, Function<String, InputStream> resourceLoader) {
+        var outputFile = new File(benchmarkReportDirectory, targetResource);
         outputFile.getParentFile().mkdirs();
-        try (InputStream in = WebsiteResourceUtils.class.getResourceAsStream(namespace + websiteResource)) {
+        try (var in = resourceLoader.apply(namespace + websiteResource)) {
             if (in == null) {
-                throw new IllegalStateException("The websiteResource (" + websiteResource
-                        + ") does not exist.");
+                throw new IllegalStateException("The websiteResource (%s) does not exist."
+                        .formatted(websiteResource));
             }
             Files.copy(in, outputFile.toPath());
         } catch (IOException e) {
-            throw new IllegalStateException("Could not copy websiteResource (" + websiteResource
-                    + ") to outputFile (" + outputFile + ").", e);
+            throw new IllegalStateException("Could not copy websiteResource (%s) to outputFile (%s)."
+                    .formatted(websiteResource, outputFile), e);
         }
     }
 
