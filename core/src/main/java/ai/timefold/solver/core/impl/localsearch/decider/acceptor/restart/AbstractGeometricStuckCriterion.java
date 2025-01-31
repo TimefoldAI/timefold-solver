67284bef4e6ec14011f78a181fe3620c2268ebbd
Fred <zepfred@gmail.com>
2025-01-31 08:42:50 -0300

    chore: address comments

--- core/src/main/java/ai/timefold/solver/core/impl/localsearch/decider/acceptor/restart/AbstractGeometricStuckCriterion.java
+++ core/src/main/java/ai/timefold/solver/core/impl/localsearch/decider/acceptor/restart/AbstractGeometricStuckCriterion.java
@@ -12,7 +12,7 @@ import org.slf4j.LoggerFactory;
 
 /**
  * Restart strategy, which exponentially increases the metric that triggers the restart process.
- * The first restart occurs after the {@code grace period + 1 * scalingFactor} metric.
+ * The first restart occurs after the {@code (gracePeriod + scalingFactor) * GEOMETRIC_FACTOR^restartCount} metric.
  * Following that, the metric increases exponentially: 1, 2, 3, 5, 7, 10, 14...
  * <p>
  * The strategy is based on the work: Search in a Small World by Toby Walsh
