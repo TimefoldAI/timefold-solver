127d9be2b2dae32234e95b566cc395c8a3ca9215
Christopher Chianelli <christopher@timefold.ai>
2025-02-18 15:59:19 -0500

    chore: start integrating the graph into the score director

--- core/src/main/java/ai/timefold/solver/core/impl/domain/variable/provided/ChangedVariableNotifier.java
+++ core/src/main/java/ai/timefold/solver/core/impl/domain/variable/provided/ChangedVariableNotifier.java
@@ -3,30 +3,37 @@ package ai.timefold.solver.core.impl.domain.variable.provided;
 import java.util.function.BiConsumer;
 
 import ai.timefold.solver.core.impl.domain.variable.descriptor.VariableDescriptor;
+import ai.timefold.solver.core.impl.score.director.InnerScoreDirector;
 
-public class ChangedVariableNotifier {
-    private final BiConsumer<VariableDescriptor<?>, Object> beforeVariableChanged;
-    private final BiConsumer<VariableDescriptor<?>, Object> afterVariableChanged;
+public class ChangedVariableNotifier<Solution_> {
+    private final BiConsumer<VariableDescriptor<Solution_>, Object> beforeVariableChanged;
+    private final BiConsumer<VariableDescriptor<Solution_>, Object> afterVariableChanged;
 
-    public ChangedVariableNotifier(BiConsumer<VariableDescriptor<?>, Object> beforeVariableChanged,
-            BiConsumer<VariableDescriptor<?>, Object> afterVariableChanged) {
+    public ChangedVariableNotifier(BiConsumer<VariableDescriptor<Solution_>, Object> beforeVariableChanged,
+            BiConsumer<VariableDescriptor<Solution_>, Object> afterVariableChanged) {
         this.beforeVariableChanged = beforeVariableChanged;
         this.afterVariableChanged = afterVariableChanged;
     }
 
-    public static ChangedVariableNotifier empty() {
-        return new ChangedVariableNotifier(
+    public static <Solution_> ChangedVariableNotifier<Solution_> empty() {
+        return new ChangedVariableNotifier<>(
                 (vd, entity) -> {
                 },
                 (vd, entity) -> {
                 });
     }
 
-    public <Entity_> void beforeVariableChanged(VariableDescriptor<?> variableDescriptor, Entity_ entity) {
+    public static <Solution_> ChangedVariableNotifier<Solution_> of(InnerScoreDirector<Solution_, ?> scoreDirector) {
+        return new ChangedVariableNotifier<>(
+                scoreDirector::beforeVariableChanged,
+                scoreDirector::afterVariableChanged);
+    }
+
+    public <Entity_> void beforeVariableChanged(VariableDescriptor<Solution_> variableDescriptor, Entity_ entity) {
         beforeVariableChanged.accept(variableDescriptor, entity);
     }
 
-    public <Entity_> void afterVariableChanged(VariableDescriptor<?> variableDescriptor, Entity_ entity) {
+    public <Entity_> void afterVariableChanged(VariableDescriptor<Solution_> variableDescriptor, Entity_ entity) {
         afterVariableChanged.accept(variableDescriptor, entity);
     }
 }
