b22d141ce5518bc3ac9e0306e1e7da9298138f35
Marek Winkler <winkler.marek@gmail.com>
2025-03-26 13:41:41 +0100

    chore: remove unused code

--- quarkus-integration/quarkus-jackson/deployment/src/main/java/ai/timefold/solver/quarkus/jackson/deployment/TimefoldJacksonProcessor.java
+++ quarkus-integration/quarkus-jackson/deployment/src/main/java/ai/timefold/solver/quarkus/jackson/deployment/TimefoldJacksonProcessor.java
@@ -1,10 +1,7 @@
 package ai.timefold.solver.quarkus.jackson.deployment;
 
 import ai.timefold.solver.jackson.api.TimefoldJacksonModule;
-import ai.timefold.solver.quarkus.jackson.TimefoldObjectMapperCustomizer;
 
-import io.quarkus.arc.deployment.AdditionalBeanBuildItem;
-import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
 import io.quarkus.jackson.spi.ClassPathJacksonModuleBuildItem;
@@ -22,10 +19,4 @@ class TimefoldJacksonProcessor {
         // https://quarkus.io/guides/rest-migration#service-loading
         return new ClassPathJacksonModuleBuildItem(TimefoldJacksonModule.class.getName());
     }
-
-    @BuildStep
-    void registerTimefoldObjectMapperCustomizer(BuildProducer<AdditionalBeanBuildItem> additionalBeans) {
-        additionalBeans.produce(new AdditionalBeanBuildItem(TimefoldObjectMapperCustomizer.class));
-    }
-
 }
