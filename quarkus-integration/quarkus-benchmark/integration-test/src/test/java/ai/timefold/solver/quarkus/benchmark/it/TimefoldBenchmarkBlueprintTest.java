315bc69dccb1089113d54f93bb7bbcad108aa000
fred <zepfred@gmail.com>
2025-05-06 19:34:25 -0300

    fix: formatting

--- quarkus-integration/quarkus-benchmark/integration-test/src/test/java/ai/timefold/solver/quarkus/benchmark/it/TimefoldBenchmarkBlueprintTest.java
+++ quarkus-integration/quarkus-benchmark/integration-test/src/test/java/ai/timefold/solver/quarkus/benchmark/it/TimefoldBenchmarkBlueprintTest.java
@@ -3,7 +3,6 @@ package ai.timefold.solver.quarkus.benchmark.it;
 import static org.apache.http.params.CoreConnectionPNames.SO_TIMEOUT;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
--- quarkus-integration/quarkus-benchmark/integration-test/src/test/java/ai/timefold/solver/quarkus/benchmark/it/TimefoldBenchmarkResourceTest.java
+++ quarkus-integration/quarkus-benchmark/integration-test/src/test/java/ai/timefold/solver/quarkus/benchmark/it/TimefoldBenchmarkResourceTest.java
@@ -2,7 +2,6 @@ package ai.timefold.solver.quarkus.benchmark.it;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
