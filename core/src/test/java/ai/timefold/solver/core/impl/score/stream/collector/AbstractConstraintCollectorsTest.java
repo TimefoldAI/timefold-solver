f339737c20a9f24065d562764879ea6a0f54102c
Lukáš Petrovický <lukas@petrovicky.net>
2025-06-10 09:03:07 +0200

    chore: fix JUnit warnings on abstract test methods

--- core/src/test/java/ai/timefold/solver/core/impl/score/stream/collector/AbstractConstraintCollectorsTest.java
+++ core/src/test/java/ai/timefold/solver/core/impl/score/stream/collector/AbstractConstraintCollectorsTest.java
@@ -8,116 +8,79 @@ import ai.timefold.solver.core.api.score.stream.common.SequenceChain;
 import ai.timefold.solver.core.impl.score.stream.collector.connected_ranges.ConnectedRangeTracker;
 import ai.timefold.solver.core.impl.score.stream.collector.consecutive.ConsecutiveSetTree;
 
-import org.junit.jupiter.api.Test;
-
 /**
  * Prescribes tests that must be implemented for every cardinality of a constraint collector.
  */
 public abstract class AbstractConstraintCollectorsTest {
 
-    @Test
     public abstract void count();
 
-    @Test
     public abstract void countLong();
 
-    @Test
     public abstract void countDistinct();
 
-    @Test
     public abstract void countDistinctLong();
 
-    @Test
     public abstract void sum();
 
-    @Test
     public abstract void sumLong();
 
-    @Test
     public abstract void sumBigDecimal();
 
-    @Test
     public abstract void sumBigInteger();
 
-    @Test
     public abstract void sumDuration();
 
-    @Test
     public abstract void sumPeriod();
 
-    @Test
     public abstract void minComparable();
 
-    @Test
     public abstract void minNotComparable();
 
-    @Test
     public abstract void maxComparable();
 
-    @Test
     public abstract void maxNotComparable();
 
-    @Test
     public abstract void average();
 
-    @Test
     public abstract void averageLong();
 
-    @Test
     public abstract void averageBigDecimal();
 
-    @Test
     public abstract void averageBigInteger();
 
-    @Test
     public abstract void averageDuration();
 
-    @Test
     public abstract void toSet();
 
-    @Test
     public abstract void toSortedSet();
 
-    @Test
     public abstract void toList();
 
-    @Test
     public abstract void toMap();
 
-    @Test
     public abstract void toMapMerged();
 
-    @Test
     public abstract void toSortedMap();
 
-    @Test
     public abstract void toSortedMapMerged();
 
-    @Test
     public abstract void conditionally();
 
-    @Test
     public abstract void compose2();
 
-    @Test
     public abstract void compose3();
 
-    @Test
     public abstract void compose4();
 
-    @Test
     public abstract void toConsecutiveSequences();
 
-    @Test
     public abstract void consecutiveUsage();
 
-    @Test
     public abstract void consecutiveUsageDynamic();
 
-    @Test
     public abstract void loadBalance();
 
-    @Test
     public abstract void collectAndThen();
 
     protected static SequenceChain<Integer, Integer> buildSequenceChain(Integer... data) {
--- core/src/test/java/ai/timefold/solver/core/impl/solver/termination/AbstractCompositeTerminationTest.java
+++ core/src/test/java/ai/timefold/solver/core/impl/solver/termination/AbstractCompositeTerminationTest.java
@@ -4,26 +4,18 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 
-import org.junit.jupiter.api.Test;
-
 abstract class AbstractCompositeTerminationTest {
 
-    @Test
     abstract void solveTermination();
 
-    @Test
     abstract void phaseTermination();
 
-    @Test
     abstract void phaseTerminationInapplicable();
 
-    @Test
     abstract void calculateSolverTimeGradientTest();
 
-    @Test
     abstract void calculatePhaseTimeGradientTest();
 
-    @Test
     abstract void calculatePhaseTimeGradientInapplicableTest();
 
     protected static <Solution_> PhaseTermination<Solution_> mockPhaseTermination() {
