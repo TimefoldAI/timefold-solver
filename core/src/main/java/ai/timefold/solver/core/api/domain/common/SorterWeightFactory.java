59ad835a2f5b84095584f112e772951344ec2219
fred <zepfred@gmail.com>
2025-09-26 17:58:56 -0300

    chore: address comments

--- core/src/main/java/ai/timefold/solver/core/api/domain/common/SorterWeightFactory.java
+++ core/src/main/java/ai/timefold/solver/core/api/domain/common/SorterWeightFactory.java
@@ -2,6 +2,9 @@ package ai.timefold.solver.core.api.domain.common;
 
 import ai.timefold.solver.core.api.domain.entity.PlanningEntity;
 import ai.timefold.solver.core.api.domain.solution.PlanningSolution;
+import ai.timefold.solver.core.config.constructionheuristic.ConstructionHeuristicPhaseConfig;
+import ai.timefold.solver.core.config.heuristic.selector.entity.EntitySelectorConfig;
+import ai.timefold.solver.core.config.heuristic.selector.value.ValueSelectorConfig;
 import ai.timefold.solver.core.impl.heuristic.move.Move;
 import ai.timefold.solver.core.impl.heuristic.selector.Selector;
 
@@ -12,13 +15,22 @@ import org.jspecify.annotations.NullMarked;
  * (a selection is a {@link PlanningEntity}, a planningValue, a {@link Move} or a {@link Selector}).
  * The selections are then sorted by their weight,
  * normally ascending unless it's configured descending.
- *
+ * The property {@code sortManner},
+ * present in the selector configurations such as {@link ValueSelectorConfig} and {@link EntitySelectorConfig},
+ * specifies how the data will be sorted.
+ * Additionally,
+ * the property {@code constructionHeuristicType} from {@link ConstructionHeuristicPhaseConfig} can also configure how entities
+ * and values are sorted.
  * <p>
  * Implementations are expected to be stateless.
  * The solver may choose to reuse instances.
  *
  * @param <Solution_> the solution type, the class with the {@link PlanningSolution} annotation
  * @param <T> the selection type
+ * 
+ * @see ValueSelectorConfig
+ * @see EntitySelectorConfig
+ * @see ConstructionHeuristicPhaseConfig
  */
 @NullMarked
 @FunctionalInterface
