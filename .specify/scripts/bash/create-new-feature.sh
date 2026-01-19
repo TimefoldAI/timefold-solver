3e5368398d4a3be68ab0759f13df3f25aacdc108
Lukáš Petrovický <lukas@petrovicky.net>
2026-01-19 07:32:59 +0100

    Move specs dir

--- .specify/scripts/bash/create-new-feature.sh
+++ .specify/scripts/bash/create-new-feature.sh
@@ -174,7 +174,7 @@ fi
 
 cd "$REPO_ROOT"
 
-SPECS_DIR="$REPO_ROOT/specs"
+SPECS_DIR="$REPO_ROOT/docs/specs"
 mkdir -p "$SPECS_DIR"
 
 # Function to generate branch name with stop word filtering and length filtering
