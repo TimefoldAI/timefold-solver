ec29583d62b4f2b2a22f30b9cdaa4169496ad968
fred <zepfred@gmail.com>
2026-02-09 12:18:15 -0300

    chore: address comments

--- persistence/jackson2/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * This module should not be used, as it will be replaced by the newer jackson module when Quarkus adopts Jackson 3.
- * Therefore, it is recommended to use timefold.solver.jackson unless Quarkus is specifically required.
- *
- * @deprecated deprecated in favor of the newer timefold.solver.jackson module.
- */
-module timefold.solver.jackson2 {
-}
