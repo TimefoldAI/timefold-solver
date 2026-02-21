546dadefbf3cb70650e44188da98761b47664ae6
Lukáš Petrovický <lukas@petrovicky.net>
2026-02-21 11:23:23 +0100

    refactor: remove use of SecurityManager

--- core/src/main/java/ai/timefold/solver/core/impl/solver/thread/DefaultSolverThreadFactory.java
+++ core/src/main/java/ai/timefold/solver/core/impl/solver/thread/DefaultSolverThreadFactory.java
@@ -4,39 +4,32 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import org.jspecify.annotations.NullMarked;
+
 /**
  * Similar to {@link Executors}'s DefaultThreadFactory, but allows settings a namePrefix.
  */
-public class DefaultSolverThreadFactory implements ThreadFactory {
+@NullMarked
+public final class DefaultSolverThreadFactory implements ThreadFactory {
 
     private static final AtomicInteger poolNumber = new AtomicInteger(1);
 
-    private final ThreadGroup group;
     private final AtomicInteger threadNumber = new AtomicInteger(1);
     private final String namePrefix;
 
-    public DefaultSolverThreadFactory() {
-        this("Solver");
-    }
-
     public DefaultSolverThreadFactory(String threadPrefix) {
-        SecurityManager s = System.getSecurityManager();
-        group = (s != null) ? s.getThreadGroup() : Thread.currentThread().getThreadGroup();
-        namePrefix = "Timefold-" + poolNumber.getAndIncrement() + "-" + threadPrefix + "-";
+        namePrefix = "Timefold-%d-%s-"
+                .formatted(poolNumber.incrementAndGet(), threadPrefix);
     }
 
     @Override
     public Thread newThread(Runnable r) {
-        Thread t = new Thread(group, r,
-                namePrefix + threadNumber.getAndIncrement(),
-                0);
-        if (t.isDaemon()) {
-            t.setDaemon(false);
-        }
-        if (t.getPriority() != Thread.NORM_PRIORITY) {
-            t.setPriority(Thread.NORM_PRIORITY);
-        }
-        return t;
+        return Thread.ofPlatform()
+                .name(namePrefix + threadNumber.getAndIncrement())
+                .group(Thread.currentThread().getThreadGroup())
+                .priority(Thread.NORM_PRIORITY)
+                .daemon(false)
+                .unstarted(r);
     }
 
 }
