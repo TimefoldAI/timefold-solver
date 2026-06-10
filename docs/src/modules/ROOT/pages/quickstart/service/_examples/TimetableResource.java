2e8a5c887e65d66b4f940219b3815b0b049092ce
Tom Cools <tom@timefold.ai>
2026-06-10 15:12:05 +0200

    docs: inline examples
    External loading doesn't work in Antora, at least not close to the source.

--- docs/src/modules/ROOT/pages/quickstart/service/_examples/TimetableResource.java
+++
@@ -1,8 +0,0 @@
-package org.acme.schooltimetabling.rest;
-
-import jakarta.ws.rs.Path;
-import ai.timefold.sdk.rest.api.ModelRest;
-
-@Path("/v1/timetables")
-public interface TimetableResource extends ModelRest {
-}
