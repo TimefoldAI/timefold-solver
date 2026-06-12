9bc0b72d45934f4984e2e9a7eebf8ec0448fd52a
rsynek <radovan.synek@gmail.com>
2026-06-12 10:48:33 +0200

    fix: rename directories originally missed

--- .github/scripts/change_versions.sh
+++ .github/scripts/change_versions.sh
@@ -10,5 +10,5 @@ echo "     New version: $NEW_VERSION"
 ./mvnw versions:set -Dfull -Dproperty=revision -DnewVersion="$NEW_VERSION" -Drevision="$NEW_VERSION" -DgenerateBackupPoms=false
 # replaces the parent revision which is not updated by the previous command
 sed -i "0,/<\/revision>/s/<revision>.*<\/revision>/<revision>$NEW_VERSION<\/revision>/" pom.xml
-sed -i "0,/<\/revision>/s/<revision>.*<\/revision>/<revision>$NEW_VERSION<\/revision>/" model/facade/model-parent/pom.xml
+sed -i "0,/<\/revision>/s/<revision>.*<\/revision>/<revision>$NEW_VERSION<\/revision>/" service/facade/service-parent/pom.xml
 git add -u
