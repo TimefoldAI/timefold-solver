055b21452cefd0dcfb216eaab2d71135d4d5228b
rsynek <radovan.synek@gmail.com>
2026-06-17 12:26:42 +0200

    fix: Revert "feat: Add effective map location to metadata events (#2321)"
    This reverts commit 71f89e11

--- service/maps/service-integration/src/main/java/ai/timefold/solver/service/maps/service/integration/internal/provider/TravelTimeAndDistanceMatrixResponse.java
+++ service/maps/service-integration/src/main/java/ai/timefold/solver/service/maps/service/integration/internal/provider/TravelTimeAndDistanceMatrixResponse.java
@@ -3,10 +3,5 @@ package ai.timefold.solver.service.maps.service.integration.internal.provider;
 import java.io.InputStream;
 import java.util.List;
 
-public record TravelTimeAndDistanceMatrixResponse(InputStream response, List<Integer> locationsOutOfMapIndexes,
-        String resolvedMapLocation) {
-
-    public TravelTimeAndDistanceMatrixResponse(InputStream response, List<Integer> locationsOutOfMapIndexes) {
-        this(response, locationsOutOfMapIndexes, null);
-    }
+public record TravelTimeAndDistanceMatrixResponse(InputStream response, List<Integer> locationsOutOfMapIndexes) {
 }
