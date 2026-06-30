786d8cf9db30e67f5d0a5bd5310a8ba3beb2315b
Christopher Chianelli <christopher@timefold.ai>
2026-06-30 09:34:27 -0400

    chore: calculate ignoreInconsistentSolution on score director

--- core/src/main/java/ai/timefold/solver/core/impl/score/director/AbstractScoreDirector.java
+++ core/src/main/java/ai/timefold/solver/core/impl/score/director/AbstractScoreDirector.java
@@ -75,7 +75,7 @@ public abstract class AbstractScoreDirector<Solution_, Score_ extends Score<Scor
     private final @Nullable LookupManager lookUpManager;
     protected final ConstraintMatchPolicy constraintMatchPolicy;
     private boolean expectShadowVariablesInCorrectState;
-    private boolean ignoreInconsistentSolutions;
+    private final boolean ignoreInconsistentSolutions;
     private final VariableDescriptorCache<Solution_> variableDescriptorCache;
     protected final ShadowVariableSupport<Solution_> shadowVariableSupport;
     private final @Nullable SolutionTracker<Solution_> solutionTracker; // Null when tracking disabled.
@@ -113,7 +113,7 @@ public abstract class AbstractScoreDirector<Solution_, Score_ extends Score<Scor
         this.lookUpManager = lookUpEnabled ? new LookupManager(solutionDescriptor.getLookUpStrategyResolver()) : null;
         this.constraintMatchPolicy = builder.constraintMatchPolicy;
         this.expectShadowVariablesInCorrectState = builder.expectShadowVariablesInCorrectState;
-        this.ignoreInconsistentSolutions = builder.ignoreInconsistentSolutions;
+        this.ignoreInconsistentSolutions = !solutionDescriptor.hasAnyShadowVariablesInconsistentMember();
         this.variableDescriptorCache = new VariableDescriptorCache<>(solutionDescriptor);
         // We set the shadow variable support,
         // which will be necessary for obtaining the change notifier
@@ -545,7 +545,7 @@ public abstract class AbstractScoreDirector<Solution_, Score_ extends Score<Scor
                 var childThreadScoreDirector =
                         scoreDirectorFactory.createScoreDirectorBuilder(environmentMode).withLookUpEnabled(lookUpEnabled)
                                 .withConstraintMatchPolicy(constraintMatchPolicy)
-                                .withIgnoreInconsistentSolutions(ignoreInconsistentSolutions).buildDerived();
+                                .buildDerived();
                 // ScoreCalculationCountTermination takes into account previous phases
                 // but the calculationCount of partitions is maxed, not summed.
                 childThreadScoreDirector.calculationCount = calculationCount;
@@ -555,7 +555,7 @@ public abstract class AbstractScoreDirector<Solution_, Score_ extends Score<Scor
                 var childThreadScoreDirector =
                         scoreDirectorFactory.createScoreDirectorBuilder(environmentMode).withLookUpEnabled(true)
                                 .withConstraintMatchPolicy(constraintMatchPolicy)
-                                .withIgnoreInconsistentSolutions(ignoreInconsistentSolutions).buildDerived();
+                                .buildDerived();
                 childThreadScoreDirector.setWorkingSolution(cloneWorkingSolution());
                 return childThreadScoreDirector;
             }
@@ -853,7 +853,7 @@ public abstract class AbstractScoreDirector<Solution_, Score_ extends Score<Scor
         // Most score directors don't need derived status; CS will override this.
         try (var uncorruptedScoreDirector = assertionScoreDirectorFactory.createScoreDirectorBuilder()
                 .withConstraintMatchPolicy(ConstraintMatchPolicy.ENABLED)
-                .withIgnoreInconsistentSolutions(ignoreInconsistentSolutions).buildDerived()) {
+                .buildDerived()) {
             uncorruptedScoreDirector.setWorkingSolution(Objects.requireNonNull(workingSolution));
             var uncorruptedInnerScore = uncorruptedScoreDirector.calculateScore();
             if (!innerScore.equals(uncorruptedInnerScore)) {
@@ -1052,11 +1052,9 @@ public abstract class AbstractScoreDirector<Solution_, Score_ extends Score<Scor
         protected ConstraintMatchPolicy constraintMatchPolicy = ConstraintMatchPolicy.DISABLED;
         protected boolean lookUpEnabled = false;
         protected boolean expectShadowVariablesInCorrectState = true;
-        protected boolean ignoreInconsistentSolutions = false;
 
         protected AbstractScoreDirectorBuilder(Factory_ scoreDirectorFactory, EnvironmentMode environmentMode) {
             this.scoreDirectorFactory = Objects.requireNonNull(scoreDirectorFactory);
-            this.environmentMode = environmentMode;
         }
 
         @SuppressWarnings("unchecked")
@@ -1074,12 +1072,6 @@ public abstract class AbstractScoreDirector<Solution_, Score_ extends Score<Scor
         @SuppressWarnings("unchecked")
         public Builder_ withExpectShadowVariablesInCorrectState(boolean expectShadowVariablesInCorrectState) {
             this.expectShadowVariablesInCorrectState = expectShadowVariablesInCorrectState;
-            return (Builder_) this;
-        }
-
-        @SuppressWarnings("unchecked")
-        public Builder_ withIgnoreInconsistentSolutions(boolean ignoreInconsistentSolutions) {
-            this.ignoreInconsistentSolutions = ignoreInconsistentSolutions;
             return (Builder_) this;
         }
 
--- core/src/main/java/ai/timefold/solver/core/impl/solver/DefaultSolverFactory.java
+++ core/src/main/java/ai/timefold/solver/core/impl/solver/DefaultSolverFactory.java
@@ -150,7 +150,6 @@ public final class DefaultSolverFactory<Solution_> implements SolverFactory<Solu
         }
         var scoreDirector = scoreDirectorFactory.createScoreDirectorBuilder(globalEnvironmentMode)
                 .withLookUpEnabled(true) // Custom phases and problem changes may rely on lookups.
-                .withIgnoreInconsistentSolutions(!solutionDescriptor.hasAnyShadowVariablesInconsistentMember())
                 .withConstraintMatchPolicy(decideConstraintMatchPolicy(solverScope, globalEnvironmentMode))
                 .build();
         solverScope.setScoreDirector(scoreDirector);
