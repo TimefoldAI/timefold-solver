b9efb77c7665160abd4552e8bd5f6b7803835ed1
Christopher Chianelli <christopher@timefold.ai>
2026-07-20 13:41:14 -0400

    chore: remove creating a new tuple for `ifExists` indictments

--- core/src/main/java/ai/timefold/solver/core/impl/bavet/common/ExistsCounter.java
+++ core/src/main/java/ai/timefold/solver/core/impl/bavet/common/ExistsCounter.java
@@ -1,33 +1,22 @@
 package ai.timefold.solver.core.impl.bavet.common;
 
-import java.util.ArrayList;
-
 import ai.timefold.solver.core.impl.bavet.common.tuple.Tuple;
 import ai.timefold.solver.core.impl.bavet.common.tuple.TupleState;
-import ai.timefold.solver.core.impl.bavet.common.tuple.indictment.IndictmentSource;
 
 public final class ExistsCounter<Tuple_ extends Tuple>
         extends AbstractPropagationMetadataCarrier<Tuple_> {
 
     final Tuple_ leftTuple;
-    final Tuple_ outTuple;
     TupleState state = TupleState.DEAD; // It's the node's job to mark a new instance as CREATING.
     int countRight = 0;
 
     ExistsCounter(Tuple_ leftTuple) {
         this.leftTuple = leftTuple;
-        if (leftTuple.getIndictmentSource() == IndictmentSource.DISABLED) {
-            outTuple = leftTuple;
-        } else {
-            outTuple = Tuple.copyOf(leftTuple);
-            outTuple.setIndictmentSource(new IndictmentSource.IndictmentSourceWithSupport(leftTuple.getIndictmentSource(),
-                    new ArrayList<>()));
-        }
     }
 
     @Override
     public Tuple_ getTuple() {
-        return outTuple;
+        return leftTuple;
     }
 
     @Override
