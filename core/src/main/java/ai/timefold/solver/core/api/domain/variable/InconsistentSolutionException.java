cd1f68140ec33b66d147addae9960c22cb9061da
Christopher Chianelli <christopher@timefold.ai>
2026-07-31 08:00:27 -0400

    chore: merge fixes

--- core/src/main/java/ai/timefold/solver/core/api/domain/variable/InconsistentSolutionException.java
+++ core/src/main/java/ai/timefold/solver/core/api/domain/variable/InconsistentSolutionException.java
@@ -1,5 +1,6 @@
 package ai.timefold.solver.core.api.domain.variable;
 
+import java.util.Collection;
 import java.util.List;
 
 import org.jspecify.annotations.NullMarked;
@@ -7,12 +8,12 @@ import org.jspecify.annotations.NullMarked;
 @NullMarked
 public class InconsistentSolutionException extends RuntimeException {
     private final Object solution;
-    private final List<Object> involvedEntityList;
+    private final Collection<Object> involvedEntityCollection;
 
-    public InconsistentSolutionException(String feature, Object solution, List<Object> involvedEntityList) {
+    public InconsistentSolutionException(String feature, Object solution, Collection<Object> involvedEntityCollection) {
         super("The solution (%s) is inconsistent. %s requires a consistent solution.".formatted(solution, feature));
         this.solution = solution;
-        this.involvedEntityList = involvedEntityList;
+        this.involvedEntityCollection = involvedEntityCollection;
     }
 
     @SuppressWarnings("unchecked")
@@ -21,7 +22,7 @@ public class InconsistentSolutionException extends RuntimeException {
     }
 
     @SuppressWarnings("unchecked")
-    public <T> List<T> getInvolvedEntityList() {
-        return (List<T>) involvedEntityList;
+    public <T> List<T> getInvolvedEntityCollection() {
+        return (List<T>) involvedEntityCollection;
     }
 }
