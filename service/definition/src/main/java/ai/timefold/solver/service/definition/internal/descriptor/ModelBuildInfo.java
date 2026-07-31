c36ec8c1754599e35c6480efb07979a6b0392fe2
rsynek <radovan.synek@gmail.com>
2026-07-31 09:37:20 +0200

    fix: return the sdkVersion to ModelBuildInfo

--- service/definition/src/main/java/ai/timefold/solver/service/definition/internal/descriptor/ModelBuildInfo.java
+++ service/definition/src/main/java/ai/timefold/solver/service/definition/internal/descriptor/ModelBuildInfo.java
@@ -1,7 +1,13 @@
 package ai.timefold.solver.service.definition.internal.descriptor;
 
+import io.quarkus.runtime.annotations.RecordableConstructor;
+
 public record ModelBuildInfo(String solverVersion, @Deprecated(forRemoval = true) String sdkVersion, String version,
         String buildTime, String branch, String buildCommit) {
+
+    @RecordableConstructor
+    public ModelBuildInfo {
+    }
 
     public ModelBuildInfo(String solverVersion, String version, String buildTime, String branch, String buildCommit) {
         this(solverVersion, solverVersion, version, buildTime, branch, buildCommit);
