103778ffcd8ec586e903196b4a407c6742d6740d
Christopher Chianelli <christopher@timefold.ai>
2026-08-13 14:26:56 -0400

    test: add persistence tests for structural scores

--- persistence/jpa/src/test/java/ai/timefold/solver/jpa/api/score/BendableBigDecimalScoreConverterTest.java
+++ persistence/jpa/src/test/java/ai/timefold/solver/jpa/api/score/BendableBigDecimalScoreConverterTest.java
@@ -21,6 +21,10 @@ class BendableBigDecimalScoreConverterTest extends AbstractScoreJpaTest {
                 BendableBigDecimalScore.of(
                         new BigDecimal[] { new BigDecimal("10000.00001"), new BigDecimal("2000.00020"),
                                 new BigDecimal("300.00300") },
+                        new BigDecimal[] { new BigDecimal("40.04000"), new BigDecimal("5.50000") }),
+                new BendableBigDecimalScore(-1L,
+                        new BigDecimal[] { new BigDecimal("10000.00001"), new BigDecimal("2000.00020"),
+                                new BigDecimal("300.00300") },
                         new BigDecimal[] { new BigDecimal("40.04000"), new BigDecimal("5.50000") }));
     }
 
