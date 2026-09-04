9173ac66482fd2b92a18699ba6f26fbec984cf8d
Marek Winkler <winkler.marek@gmail.com>
2026-09-04 11:31:52 +0200

    chore: change TransportType to enum to quick fix OpenAPI spec

--- service/maps/api/src/main/java/ai/timefold/solver/service/maps/api/model/TransportType.java
+++ service/maps/api/src/main/java/ai/timefold/solver/service/maps/api/model/TransportType.java
@@ -2,20 +2,36 @@ package ai.timefold.solver.service.maps.api.model;
 
 import java.util.Objects;
 
-public record TransportType(String value) {
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
-    public static final TransportType CAR = new TransportType("car");
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
 
-    public TransportType {
+@Schema(description = "The type of transport used (car, bike, ... ) supported by Timefold.")
+public enum TransportType {
+
+    CAR("car");
+
+    private final String value;
+
+    TransportType(String value) {
         Objects.requireNonNull(value, "TransportType value must not be null.");
         value = value.trim().toLowerCase();
         if (value.isEmpty()) {
             throw new IllegalArgumentException("TransportType value must not be blank.");
         }
+        this.value = value;
     }
 
+    @JsonCreator
     public static TransportType of(String value) {
-        return new TransportType(value);
+        Objects.requireNonNull(value, "TransportType value must not be null.");
+        return TransportType.valueOf(value.trim().toUpperCase());
+    }
+
+    @JsonValue
+    public String value() {
+        return value;
     }
 
     @Override
