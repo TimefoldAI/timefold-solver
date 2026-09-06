afa0c930057b10ee7a778e9456c834b25b6771f0
Lukas Petrovicky <lukas@timefold.ai>
2026-09-06 12:28:30 +0200

    Fix Javadocs

--- core/src/main/java/ai/timefold/solver/core/preview/api/neighborhood/stream/picking/BiPickingStream.java
+++ core/src/main/java/ai/timefold/solver/core/preview/api/neighborhood/stream/picking/BiPickingStream.java
@@ -1,13 +1,23 @@
 package ai.timefold.solver.core.preview.api.neighborhood.stream.picking;
 
 import ai.timefold.solver.core.preview.api.neighborhood.BiMoveConstructor;
+import ai.timefold.solver.core.preview.api.neighborhood.UniMoveConstructor;
 import ai.timefold.solver.core.preview.api.neighborhood.stream.MoveStream;
 
 import org.jspecify.annotations.NullMarked;
 
+/**
+ * A two-value variant of {@link PickingStream}.
+ *
+ * @param <Solution_>
+ * @param <A>
+ */
 @NullMarked
 public interface BiPickingStream<Solution_, A, B> extends PickingStream {
 
+    /**
+     * As defined by {@link UniPickingStream#asMove(UniMoveConstructor)}.
+     */
     MoveStream<Solution_> asMove(BiMoveConstructor<Solution_, A, B> moveConstructor);
 
 }
--- core/src/main/java/ai/timefold/solver/core/preview/api/neighborhood/stream/picking/UniPickingStream.java
+++ core/src/main/java/ai/timefold/solver/core/preview/api/neighborhood/stream/picking/UniPickingStream.java
@@ -1,5 +1,7 @@
 package ai.timefold.solver.core.preview.api.neighborhood.stream.picking;
 
+import ai.timefold.solver.core.preview.api.move.Move;
+import ai.timefold.solver.core.preview.api.move.builtin.Moves;
 import ai.timefold.solver.core.preview.api.neighborhood.UniMoveConstructor;
 import ai.timefold.solver.core.preview.api.neighborhood.stream.MoveStream;
 import ai.timefold.solver.core.preview.api.neighborhood.stream.enumerating.UniEnumeratingStream;
@@ -7,35 +9,73 @@ import ai.timefold.solver.core.preview.api.neighborhood.stream.joiner.BiNeighbor
 
 import org.jspecify.annotations.NullMarked;
 
+/**
+ * A single-value variant of {@link PickingStream}.
+ *
+ * @param <Solution_>
+ * @param <A>
+ */
 @NullMarked
 public interface UniPickingStream<Solution_, A> extends PickingStream {
 
+    /**
+     * A just-in-time join of two enumerating streams,
+     * without any restrictions.
+     *
+     * @param uniEnumeratingStream stream to join this stream with
+     * @return the joined stream, which is lazily evaluated when iterated over
+     * @param <B>
+     */
     @SuppressWarnings("unchecked")
     default <B> BiPickingStream<Solution_, A, B> pick(UniEnumeratingStream<Solution_, B> uniEnumeratingStream) {
         return pick(uniEnumeratingStream, new BiNeighborhoodsJoiner[0]);
     }
 
+    /**
+     * As defined by {@link UniPickingStream#pick(UniEnumeratingStream, BiNeighborhoodsJoiner...)}.
+     */
     @SuppressWarnings("unchecked")
     default <B> BiPickingStream<Solution_, A, B> pick(UniEnumeratingStream<Solution_, B> uniEnumeratingStream,
             BiNeighborhoodsJoiner<A, B> joiner) {
         return pick(uniEnumeratingStream, new BiNeighborhoodsJoiner[] { joiner });
     }
 
+    /**
+     * As defined by {@link UniPickingStream#pick(UniEnumeratingStream, BiNeighborhoodsJoiner...)}.
+     */
     @SuppressWarnings("unchecked")
     default <B> BiPickingStream<Solution_, A, B> pick(UniEnumeratingStream<Solution_, B> uniEnumeratingStream,
             BiNeighborhoodsJoiner<A, B> joiner1, BiNeighborhoodsJoiner<A, B> joiner2) {
         return pick(uniEnumeratingStream, new BiNeighborhoodsJoiner[] { joiner1, joiner2 });
     }
 
+    /**
+     * As defined by {@link UniPickingStream#pick(UniEnumeratingStream, BiNeighborhoodsJoiner...)}.
+     */
     @SuppressWarnings("unchecked")
     default <B> BiPickingStream<Solution_, A, B> pick(UniEnumeratingStream<Solution_, B> uniEnumeratingStream,
             BiNeighborhoodsJoiner<A, B> joiner1, BiNeighborhoodsJoiner<A, B> joiner2, BiNeighborhoodsJoiner<A, B> joiner3) {
         return pick(uniEnumeratingStream, new BiNeighborhoodsJoiner[] { joiner1, joiner2, joiner3 });
     }
 
+    /**
+     * A just-in-time join of two enumerating streams,
+     * where only the values that match the joiners will be matched.
+     *
+     * @param uniEnumeratingStream stream to join this stream with
+     * @return the joined stream, which is lazily evaluated when iterated over
+     * @param <B>
+     */
     <B> BiPickingStream<Solution_, A, B> pick(UniEnumeratingStream<Solution_, B> uniEnumeratingStream,
             BiNeighborhoodsJoiner<A, B>... joiners);
 
+    /**
+     * Converts this stream into a {@link MoveStream},
+     * which will produce a {@link Move} for each value in this stream.
+     *
+     * @param moveConstructor move constructor to use; see {@link Moves}.
+     * @return the move stream, which is lazily evaluated when iterated over
+     */
     MoveStream<Solution_> asMove(UniMoveConstructor<Solution_, A> moveConstructor);
 
 }
