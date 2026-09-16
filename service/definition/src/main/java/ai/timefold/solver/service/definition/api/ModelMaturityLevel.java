e862e73c4a7b892157bcf3f79dd57ebb690becf9
rsynek <radovan.synek@gmail.com>
2026-09-16 13:51:55 +0200

    Make deserialization backward compatible

--- service/definition/src/main/java/ai/timefold/solver/service/definition/api/ModelMaturityLevel.java
+++ service/definition/src/main/java/ai/timefold/solver/service/definition/api/ModelMaturityLevel.java
@@ -1,7 +1,10 @@
 package ai.timefold.solver.service.definition.api;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+
 public enum ModelMaturityLevel {
 
+    @JsonAlias("Example") // To ensure backward compatibility with already built models.
     Template,
     Experimental,
     Preview,
